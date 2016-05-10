9e47fdb669fe900a13d7748b2f3469ba77df4952
jamespino <james.c.pino@vanderbilt.edu>
2016-05-10 11:34:19 -0500

    Adding analysis functions

---
+++ Analysis/go_analysis.py
@@ -0,0 +1,303 @@
+import matplotlib
+import numpy as np
+
+matplotlib.use('Qt4Agg')
+from orangecontrib.bio import go
+import matplotlib.pyplot as plt
+import mpl_toolkits.axes_grid1 as axgrid
+import scipy.cluster.hierarchy as sch
+import os
+from textwrap  import wrap
+
+class GoAnalysis:
+    """
+    Go analysis class.
+    Uses the orangecontrib.bio package
+    """
+
+    def __init__(self, species='hsa', slim=False, output_directory='tmp', reference=None):
+        self.ontology = go.Ontology()
+        self.slim = False
+        self.reference = reference
+        if slim:
+            self.slim = True
+            self.slim_name = slim
+            self.ontology.set_slims_subset(slim)  # goslim_pir goslim_generic
+        self.annotations = go.Annotations(species, ontology=self.ontology)
+        self.global_go = {}
+        self.out_dir = output_directory
+        if not os.path.exists(self.out_dir):
+            os.mkdir(self.out_dir)
+
+    def create_array_per_time(self, data, aspect='F'):
+        """
+
+        :param data:
+        :param aspect:
+        :param slims:
+        :return:
+        """
+        res = self.annotations.get_enriched_terms(data, slims_only=self.slim, aspect=aspect, reference=self.reference)
+        # Slims goslim_chembl
+        sorted_list = np.ones((len(res.items()), 4), dtype='S50')
+        sorted_list[:, 2].astype('float')
+        n = 0
+        for go_id, (genes, p_value, ref) in res.items():
+            if self.slim:
+                pass
+            elif ref < 10.:
+                continue
+
+            sorted_list[n, 0] = self.ontology[go_id].name
+            sorted_list[n, 1] = go_id
+            if p_value > 0.05:
+                continue
+                sorted_list[n, 2] = float(0)
+            else:
+                # sorted_list[n, 2] = float(len(genes) / float(ref)) * 100.
+                sorted_list[n, 2] = -1. * np.log10(p_value)
+            print(
+            go_id, float(len(genes) / float(ref)) * 100, self.ontology[go_id].name, p_value, len(genes), ref, genes)
+            self.global_go[go_id] = self.ontology[go_id].name
+            sorted_list[n, 3] = str(genes)
+            n += 1
+        if n == 0:
+            print("No significant p-values")
+        return sorted_list[:n - 1, :]
+
+    def create_data_set(self, list_of_exp, aspect):
+        """
+
+        :param list_of_exp:
+        :param aspect:
+        :return:
+        """
+        num_of_lists = len(list_of_exp)
+        results = []
+        for i in list_of_exp:
+            tmp_array = self.create_array_per_time(i, aspect)
+            results.append(tmp_array)
+        all_terms = []
+        for each in results:
+            all_terms.extend(list(each[:, 1]))
+        all_terms = set(all_terms)
+        data_set = np.zeros((len(all_terms), num_of_lists + 1), dtype='S50')
+        for n, i in enumerate(all_terms):
+            data_set[n, 0] = i
+            for num in range(1, num_of_lists + 1):
+                data_set[n, num] = return_go_number(i, results[num - 1])
+        return data_set
+
+    def find_terms(self, data, term):
+        """
+
+        :param data:
+        :param term:
+        :return:
+        """
+        shape = np.shape(data)[1]
+        new_data = np.zeros((len(data), shape), dtype=('S50'))
+        new_names = data[:, 0]
+        n = 0
+        for i in range(len(new_names)):
+            if len(self.global_go[new_names[i]].split(term)) < 2:
+                continue
+            new_data[n, :] = data[i, :]
+            n += 1
+        return new_data[:n - 1, :]
+
+    def plot_heatmap(self, data, all_names, start, stop, savename, labels):
+        """
+
+        :param data:
+        :param all_names:
+        :param start:
+        :param stop:
+        :param savename:
+        :return:
+        """
+        matrix = data[start:stop, :]
+        size_of_data = np.shape(data)[1]
+        length_matrix = len(matrix)
+        fig = plt.figure(figsize=(6,12))
+        ax1 = fig.add_subplot(111)
+        # plt.title(savename)
+        im = ax1.imshow(matrix, aspect=.25, interpolation='nearest', extent=(0, size_of_data, 0, length_matrix + 1),
+                        origin='lower')
+        plt.colorbar(im)
+        names_2 = []
+        for i in all_names[start:stop]:
+            names_2.append(self.global_go[i])
+        y_ticks = np.linspace(.5, length_matrix + .5, length_matrix)
+        plt.yticks(y_ticks, names_2, fontsize=16)
+        x_ticks = np.linspace(.5, size_of_data - .5, size_of_data)
+        if labels:
+            print(x_ticks)
+            plt.xticks(x_ticks, labels, fontsize=16, rotation='90')
+        else:
+            print("Provide labels")
+
+        # divider = axgrid.make_axes_locatable(ax1)
+        # cax = divider.append_axes("top", size="5%", pad=0.3)
+        # color_bar = fig.colorbar(im, cax=cax, orientation='horizontal')
+        # color_bar.set_label('% change', labelpad=-40, y=0.45)
+        plt.savefig(os.path.join(self.out_dir, '%s.pdf' % savename), dpi=300, bbox_inches='tight')
+        plt.close()
+
+    def analysis_data(self, data, aspect='F', savename='tmp', labels=None, analyze=True):
+        """
+
+        :param data:
+        :param aspect:
+        :param savename:
+        :param labels:
+        :return:
+        """
+        data = self.create_data_set(data, aspect)
+        # self.print_ranked_over_time(data)
+        names, array = sort_data(data)
+        self.names = names
+        self.array = array
+        if not analyze:
+            return
+        if self.slim:
+            savename += "_%s" % self.slim_name
+        savename += '_%s' % aspect
+
+        self.plot_heatmap(array, names, 0, 50, '%s_top' % savename, labels)
+        self.plot_heatmap(array, names, -50, -1, '%s_bottom' % savename, labels)
+        D = array[:, :]
+        fig = plt.figure()
+        axdendro = fig.add_axes([0.09, 0.1, 0.2, 0.8])
+        Y = sch.linkage(D, method='centroid')
+        Z = sch.dendrogram(Y, orientation='right')
+        axdendro.set_xticks([])
+        axdendro.set_yticks([])
+        axmatrix = fig.add_axes([0.3, 0.1, 0.6, 0.8])
+        index = Z['leaves']
+        D = D[index, :]
+        N = names[index]
+        im = axmatrix.matshow(D, aspect='auto', origin='lower')
+        axmatrix.set_xticks([])
+        axmatrix.set_yticks([])
+        axcolor = fig.add_axes([0.91, 0.1, 0.02, 0.8])
+        plt.colorbar(im, cax=axcolor)
+        fig.savefig(os.path.join(self.out_dir, '%s_dendrogram.pdf' % savename))
+        self.plot_heatmap(D, N, 0, 50, '%s_top_dendro' % savename, labels)
+        self.plot_heatmap(D, N, -50, -1, '%s_bottom_dendro' % savename, labels)
+
+    def print_ranked_over_time(self, data):
+        """ Prints information about top hits
+
+        :param data:
+        :return:
+        """
+        for i in range(0, np.shape(data)[1] - 2):
+            print(i)
+            names, tmp = sort_data_by_index(data, i)
+            for j in range(len(data), len(data) - 10):
+                print(i, self.global_go[names[j]], tmp[j, 1 + i] - tmp[j, i])
+
+    def find_and_plot_subterms(self,term,savename):
+        print(term)
+        terms = self.ontology.extract_sub_graph(term)
+        for i in terms:
+            if i in self.global_go:
+                print(i, self.global_go[i])
+        self.plot_specific_GO(terms, savename)
+
+    def plot_specific_GO(self, term, savename):
+        """ Plots a scatter plot of selected terms over time
+
+        :param names:
+        :param data:
+        :param term:
+        :param savename:
+        :return:
+        """
+        found_terms = []
+        for i in term:
+            if i in self.names:
+                #print(self.ontology.term_depth(i),i)
+                found_terms.append(i)
+            # else:
+            #    print("%s is not found" % i)
+        num_colors = len(found_terms)
+        if num_colors == 0:
+            print("Did not find any of the terms")
+            return
+        fig = plt.figure()
+        ax = plt.subplot(111)
+        cm = plt.get_cmap('jet')
+
+        ax.set_color_cycle([cm(1. * i / num_colors) for i in range(num_colors)])
+        x = [1, 6, 24, 48]
+        for i in found_terms:
+            y_index = np.where(self.names == i)
+
+            y = self.array[y_index][0]
+            label = "\n".join(wrap(self.global_go[i], 40))
+            ax.plot(x, y, 'o-', label=label)
+        plt.ylabel('-log(p-value)', fontsize=16)
+        plt.xlabel('Time(hr)', fontsize=16)
+        #ax.legend(loc=0, bbox_to_anchor=(0.5, 1.05),
+        #          ncol=1, fancybox=True, shadow=True)
+        #box = ax.get_position()
+        #ax.set_position([box.x0, box.y0 + box.height * 0.1,
+        #                  box.width, box.height * 0.9])
+        #ax.legend(loc='center', bbox_to_anchor=(0.5, -0.15),
+        #          fancybox=True, shadow=True, ncol=5)
+        # #plt.legend(loc=0)
+        #plt.tight_layout()
+        handles, labels = ax.get_legend_handles_labels()
+        lgd = ax.legend(handles, labels, loc='best', bbox_to_anchor=(1.01, 1.0))
+        fig.savefig('%s.png' % savename, bbox_extra_artists=(lgd,), bbox_inches='tight')
+        plt.show()
+
+
+def return_go_number(go_term, go_array):
+    """
+
+    :param go_term:
+    :param go_array:
+    :return:
+    """
+    if go_term in list(go_array[:, 1]):
+        for i in range(len(go_array[:, 0])):
+            if go_term == go_array[i, 1]:
+                return go_array[i, 2]
+    else:
+        return 0
+
+
+def sort_data(data):
+    """
+
+    :param data:
+    :return:
+    """
+    names = data[:, 0].copy()
+    array = data[:, 1:].astype(np.float32).copy()
+    # step_size = np.average(array,axis=1).argsort()
+    # step_size = (array[:, 2] - array[:, 1]).argsort()
+    step_size = (array[:, -1]).argsort()
+    names = names[step_size]
+    array = array[step_size]
+    # array_2 = array[:,1:] - array[:,:-1]
+    return names, array
+
+
+def sort_data_by_index(data, index):
+    """
+
+    :param index:
+    :param data:
+    :return:
+    """
+    names = data[:, 0].copy()
+    array = data[:, 1:].astype(np.float32).copy()
+    # step_size = np.average(array,axis=1).argsort()
+    step_size = (array[:, index + 1] - array[:, index]).argsort()
+    names = names[step_size]
+    array = array[step_size]
+    return names, array
---
+++ Analysis/network_subgraphs.py
@@ -0,0 +1,336 @@
+import networkx as nx
+import pygraphviz as pyg
+
+from network_tools import compress_edges
+import os
+
+
+class NetworkSubgraphs:
+    """ Class to create subgraphs of larger network
+
+    """
+    def __init__(self, network, exp_data):
+        self.network = network
+        self.nodes = self.network.nodes()
+        self.exp_data = exp_data
+
+    def generate_shortest_paths_between_two_proteins(self, protein_1, protein_2):
+        """
+        Generates a graph based on all shortest paths between two proteins
+        :param protein_1:
+        :param protein_2:
+        :return: graph
+        """
+        graph = pyg.AGraph(directed=True)
+        if not nx.has_path(self.network, protein_1, protein_2):
+            print("Path does NOT exist between %s and %s" % (protein_1, protein_2))
+        else:
+            for path in nx.all_shortest_paths(self.network, protein_1, protein_2):
+                self.add_edges_from_path(graph, path)
+        if not nx.has_path(self.network, protein_2, protein_1):
+            print("Path does NOT exist between %s and %s" % (protein_2, protein_1))
+        else:
+            for path in nx.all_shortest_paths(self.network, protein_2, protein_1):
+                self.add_edges_from_path(graph, path)
+        #self.create_legend(graph)
+        self.write(graph)
+        graph.write("%s_and_%s.dot" % (protein_1, protein_2))
+        graph.draw("%s_and_%s.pdf" % (protein_1, protein_2), prog='dot')
+        return graph
+
+    def generate_shortest_paths_between_lists_of_proteins(self, protein_list,savename):
+        """
+        Generates a graph based on all shortest paths between two proteins
+        :param protein_1:
+        :param protein_2:
+        :return: graph
+        """
+        graph = pyg.AGraph(directed=True)
+        for protein_1 in protein_list:
+            for protein_2 in protein_list:
+                if protein_1 == protein_2:
+                    continue
+                else:
+                    if not nx.has_path(self.network, protein_1, protein_2):
+                        print("Path does NOT exist between %s and %s" % (protein_1, protein_2))
+                    else:
+                        for path in nx.all_shortest_paths(self.network, protein_1, protein_2):
+                            self.add_edges_from_path(graph, path)
+                    if not nx.has_path(self.network,  protein_2,protein_1):
+                            print("Path does NOT exist between %s and %s" % (protein_2, protein_1))
+                    else:
+                        for path in nx.all_shortest_paths(self.network, protein_2, protein_1):
+                            self.add_edges_from_path(graph, path)
+        # self.create_legend(graph)
+        self.write(graph)
+        graph.write("%s.dot" % savename)
+        graph.draw("%s.pdf" % savename, prog='dot')
+        return graph
+
+    #TODO create a function that finds all neighbors of each proteins and links them
+    # THis would find more than the shortest path
+    def generate_all_paths_between_two_proteins(self, protein_1, protein_2):
+        """
+        Generates a graph based on all shortest paths between two proteins
+        :param protein_1:
+        :param protein_2:
+        :return: graph
+        """
+        graph = pyg.AGraph(directed=True)
+        if not nx.has_path(self.network, protein_1, protein_2):
+            print("Path does NOT exist between %s and %s" % (protein_1, protein_2))
+            return graph
+        if not nx.has_path(self.network, protein_2, protein_1):
+            print("Path does NOT exist between %s and %s" % (protein_2, protein_1))
+            return graph
+        graph = pyg.AGraph(directed=True)
+        for path in nx.all_shortest_paths(self.network, protein_1, protein_2):
+            self.add_edges_from_path(graph, path)
+        for path in nx.all_shortest_paths(self.network, protein_2, protein_1):
+            self.add_edges_from_path(graph, path)
+        self.create_legend(graph)
+        graph.write("%s_and_%s.dot" % (protein_1, protein_2))
+        graph.draw("%s_and_%s.pdf" % (protein_1, protein_2), prog='dot')
+        return graph
+
+    def generate_shortest_paths_from_protein_to_protein(self, protein_1, protein_2):
+        """
+        Generates a graph from all the shortests paths FROM protein 1 to protein 2
+        :param protein_1: string
+        :param protein_2: string
+        :return:
+        """
+        graph = pyg.AGraph(directed=True)
+        if not nx.has_path(self.network, protein_1, protein_2):
+            print("Path does NOT exist between %s and %s" % (protein_1, protein_2))
+            return graph
+        short_distance = nx.shortest_path_length(self.network, protein_1, protein_2)
+        print("Shortest path length from %s to %s = %i" % (protein_1, protein_2, short_distance))
+
+        for path in nx.all_shortest_paths(self.network, protein_1, protein_2):
+            self.add_edges_from_path(graph, path)
+        #self.create_legend(graph)
+        graph.write("%s_to_%s.dot" % (protein_1, protein_2))
+        graph.draw("%s_to_%s.pdf" % (protein_1, protein_2), prog='dot')
+        return graph
+
+    def generate_paths_between_proteins_list(self, protein_list, save_name):
+        """ Generate s graph from paths from a list of pair of proteins
+
+        :param protein_list:
+        :param save_name:
+        :return:
+        """
+        protein_pair_graph = pyg.AGraph(directed=True)
+        for pair in protein_list:
+            print(pair)
+            graph = self.generate_shortest_paths_from_protein_to_protein(pair[0],pair[1])
+            for node in graph.nodes():
+                print(node)
+                protein_pair_graph.add_node(node, **self.network.node[node])
+            for edge in graph.edges():
+                protein_pair_graph.add_edge(edge[0],edge[1],**self.network.edge[edge[0]][edge[1]])
+        protein_pair_graph.draw("%s.pdf" % save_name, prog='dot')
+        return protein_pair_graph
+
+
+    def generate_upstream_network_of_protein(self, protein_1, savename='test',compress=False):
+        """
+        Finds all linear pathways leading up to a protein.
+        :param protein_1: string
+        :param savename: string
+        :return: graph
+        """
+        paths = []
+        for i in self.nodes:
+            if i in self.nodes:
+                if nx.has_path(self.network, i, protein_1):
+                    path = nx.shortest_path(self.network, i, protein_1)
+                    paths.append(path)
+        temporary_graph = pyg.AGraph(directed=True)
+        for path in paths:
+            cont = True
+            for protein in list(path):
+                if protein not in self.exp_data.sign_proteins_omics+self.exp_data.metabolomics:
+                    cont = False
+            if not cont:
+                continue
+            self.add_edges_from_path(temporary_graph, path)
+        if compress:
+            temporary_graph = compress_edges(temporary_graph)
+        #self.create_legend(temporary_graph)
+        temporary_graph.write('%s.dot' % savename)
+        temporary_graph.draw('%s.pdf' % savename, prog='dot')
+        return temporary_graph
+
+    def generate_downstream_network_of_protein(self, network, protein_1, exp_data, savename='test', compress=False):
+        """
+        Finds all linear pathways leading up to a protein.
+        :param protein_1: string
+        :param savename: string
+        :return: graph
+        """
+        paths = []
+        nodes = network.nodes()
+        for i in nodes:
+            if i in nodes:
+                if nx.has_path(network, protein_1,i):
+                    path = nx.shortest_path(network, protein_1,i)
+                    paths.append(path)
+        temporary_graph = pyg.AGraph(directed=True)
+        for path in paths:
+            cont = True
+            for protein in list(path):
+                if protein not in self.exp_data.measured:
+                    cont = False
+            if not cont:
+                continue
+            self.add_edges_from_path(temporary_graph, path)
+        if compress:
+            temporary_graph = compress_edges(temporary_graph)
+        #self.create_legend(temporary_graph)
+        temporary_graph.write('%s.dot' % savename)
+        temporary_graph.draw('%s.pdf' % savename, prog='dot')
+        return temporary_graph
+
+    def write(self,network):
+        dict_of_types = {'activation': 'onormal',
+                         'indirect effect': 'odiamondodiamond',
+                         'expression': 'normal',
+                         'inhibition': 'tee',
+                         'binding/association': 'curve',
+                         'phosphorylation': 'dot',
+                         'missing interaction': 'odiamond',
+                         'compound': 'dotodot',
+                         'dissociation': 'diamond',
+                         'ubiquitination': 'oldiamond',
+                         'state change': 'teetee',
+                         'dephosphorylation': 'onormal',
+                         'repression': 'obox',
+                         'glycosylation': 'dot'}
+        activators = ['activation', 'expression','phosphorylation']
+        inhibitors = ['inhibition','repression','dephosphorylation']
+        physical_contact = ['binding/association','dissociation','state change']
+        chemical = ['compound','glycosylation','ubiquitination']
+        for i in network.edges():
+            n = network.get_edge(i[0],i[1])
+            edge_type = str(i.attr['interactionType'])
+            cont = False
+            for j in activators:
+                if edge_type.startswith(j):
+                    n.attr['arrowhead'] = 'normal'
+                    cont = True
+            if cont:
+                continue
+            for j in inhibitors:
+                if edge_type.startswith(j):
+                    n.attr['arrowhead'] = 'tee'
+                    cont = True
+            if cont:
+                continue
+            for j in physical_contact:
+                if edge_type.startswith(j):
+                    n.attr['arrowhead'] = 'diamond'
+                    cont = True
+            if cont:
+                continue
+            for j in chemical:
+                if edge_type.startswith(j):
+                    n.attr['arrowhead'] = 'dotodot'
+                    cont = True
+            if cont:
+                continue
+            else:
+                print(n,n.attr)
+
+
+
+    def paint_network(self, graph, list_to_paint, color):
+        """
+        Paints a graph given a list of nodes and a color for that list
+        :param graph: pygraphvix.AGraph
+        :param list_to_paint: list
+        :param color: string
+        :return:
+        """
+        tmp_g = graph.copy()
+        nodes1 = tmp_g.nodes()
+        for i in list_to_paint:
+            if i in nodes1:
+                n = tmp_g.get_node(i)
+                n.attr['measured'] = 'True'
+                n.attr['color'] = 'black'
+                n.attr['fillcolor'] = color
+                n.attr['style'] = 'filled'
+        return tmp_g
+
+    def paint_network_overtime(self, graph, list_of_lists, color_list,savename):
+        """
+
+        :param graph:
+        :param list_of_lists:
+        :param color_list:
+        :return:
+        """
+        if len(list_of_lists) != len(color_list):
+            print('Length of list of data must equal len of color list')
+        string = 'convert '
+        tmp_graph = graph.copy()
+        for n, i in enumerate(list_of_lists):
+            graph2 = self.paint_network(tmp_graph, i, color_list[n])
+            self.write(graph2)
+            graph2.draw('%s_%04i.png' % (savename, n), prog='dot')
+            string += '%s_%04i.png ' % (savename, n)
+        string += '  %s.pdf' % savename
+        print(string)
+        os.system(string)
+
+    def add_edges_from_path(self, graph, path):
+        """
+        Adds a path to a graph by extracting the edges and edge attributes
+        from the ddn
+        :param graph:
+        :param path:
+        :return: None
+        """
+        previous = None
+        for protein in list(path):
+            if previous is None:
+                previous = protein
+                continue
+            else:
+                graph.add_edge(previous, protein, **self.network.edge[previous][protein])
+                previous = protein
+
+
+    def create_legend(self, graph):
+        """
+        adds a legend to a graph
+        :param graph:
+        :return:
+        """
+        dict_of_types = {'activation': 'onormal',
+                         'indirect effect': 'odiamondodiamond',
+                         'expression': 'normal',
+                         'inhibition': 'tee',
+                         'binding/association': 'curve',
+                         'phosphorylation': 'dot',
+                         'missing interaction': 'odiamond',
+                         'compound': 'dotodot',
+                         'dissociation': 'diamond',
+                         'ubiquitination': 'oldiamond',
+                         'state change': 'teetee',
+                         'dephosphorylation': 'onormal',
+                         'repression': 'obox',
+                         'glycosylation': 'dot'}
+        subgraph = []
+        len_dic = len(dict_of_types)
+        for n, i in enumerate(dict_of_types):
+            subgraph.append(n)
+            subgraph.append(n + len_dic)
+            graph.add_node(n, label="")
+            graph.add_node(n + len_dic, label="")
+            graph.add_edge(n, n + len_dic, dir='both', arrowhead=dict_of_types[i], arrowtail="none", label=i)
+        graph.add_subgraph(subgraph, name='cluster_legend', rank="LR")
+
+
