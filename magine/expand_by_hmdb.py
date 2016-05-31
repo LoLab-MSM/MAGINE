9756703763225d9546179e5378dcc21f3cc6e071
jamespino <james.c.pino@vanderbilt.edu>
2016-05-31 15:29:27 -0500

    Creating expand_by_hmdb function. Takes a list of HMDB identifiers and a graph. Add proteins in the ChemicalMapper class.

---
+++ magine/expand_by_hmdb.py
@@ -0,0 +1,88 @@
+from Mappings.maps import cm
+
+
+def expand_by_hmdb(graph, metabolite_list, all_measured):
+    tmp_graph = graph.copy()
+    start_nodes = tmp_graph.nodes()
+    start_edges = tmp_graph.edges()
+    missing_metabolites = []
+    for i in metabolite_list:
+        if i not in start_nodes:
+            missing_metabolites.append(i)
+
+    count_in_network, count_not_in_network = 0, 0
+    missing_edge = 0
+    protein_hits = 0
+    added_proteins = []
+    missing_protein_info = 0
+    missing_proteins = set()
+    for i in metabolite_list:
+        if i in tmp_graph.nodes():
+            count_in_network += 1
+            if i in cm.hmdb_accession_to_protein:
+                tmp_list = cm.hmdb_accession_to_protein[i][0]
+                if tmp_list is None:
+                    missing_protein_info += 1
+                    continue
+                else:
+                    for each in tmp_list:
+                        if each is None:
+                            continue
+                        elif each not in tmp_graph.nodes():
+                            added_proteins.append(each)
+                        missing_edge += 1
+                        tmp_graph.add_edge(each, i)
+
+        else:
+            count_not_in_network += 1
+            if i in cm.hmdb_accession_to_protein:
+                tmp_list = cm.hmdb_accession_to_protein[i][0]
+                if tmp_list is None:
+                    missing_protein_info += 1
+                    continue
+                else:
+                    for each in tmp_list:
+                        if each is None:
+                            pass
+                        elif each in start_nodes:
+                            missing_edge += 1
+                            tmp_graph.add_edge(i, each)
+                            protein_hits += 1
+                        else:
+                            missing_proteins.add(each)
+    end_nodes = tmp_graph.nodes()
+    end_edges = tmp_graph.edges()
+    metabolites_added = []
+    still_missing = []
+    for i in missing_metabolites:
+        if i in end_nodes:
+            metabolites_added.append(i)
+        else:
+            still_missing.append(i)
+
+    count = 0
+    for each in added_proteins:
+        if each in all_measured:
+            count += 1
+
+    # print(missing_proteins)
+    print('Metabolites not in starting network = {0}'.format(len(missing_metabolites)))
+    print('Metabolites added to network = {0}'.format(len(metabolites_added)))
+    print('Metabolites still not in network = {0}'.format(len(still_missing)))
+    print('\n')
+    print('Before number of nodes = {0}'.format(len(start_nodes)))
+    print('Before number of edges = {0}'.format(len(start_edges)))
+    print('\n')
+    print('After number of nodes = {0}'.format(len(end_nodes)))
+    print('After number of edges = {0}'.format(len(end_edges)))
+    print('\n')
+    print('Added nodes = {0}'.format(len(end_nodes) - len(start_nodes)))
+    print('Added edges = {0}'.format(len(end_edges) - len(start_edges)))
+    print('\n')
+    print('Number of proteins not in KEGG = {0}'.format(len(missing_proteins)))
+    print('Number of add proteins = {0}'.format(len(added_proteins)))
+    print('Proteins added that were measured = {0}'.format(count))
+    print('\n')
+    print('missing metabolite-protein info = {0}'.format(missing_protein_info))
+
+    return tmp_graph
