62192db93174740f60ffffe1e9c1028f82319d55
jamespino <james.c.pino@vanderbilt.edu>
2016-10-26 17:56:08 -0500

    Reformatted file.
    Also adding download all of kegg. I think it is going to be better to download all of kegg each time, then search in a dictionary, rather than search each protein individually like it does now.
    Currently, old functionality remains. New method not fully implemented

--- magine/network_generator.py
+++ magine/network_generator.py
@@ -18,7 +18,8 @@ except KeyError:
     kegg = KEGG()
 
 
-def find_kegg_pathways(protein_names, num_overlap=1, download=True, species='hsa', overwrite=True):
+def find_kegg_pathways(protein_names, num_overlap=1, download=True,
+                       species='hsa', overwrite=True):
     """
     find_kegg_pathways(protein_names,num_overlap,download)
     Find pathways associated with a list of genes.
@@ -69,7 +70,8 @@ def delete_disconnected_network(full_graph, verbose=False):
     :return:
     """
     tmp_g = full_graph.to_undirected()
-    sorted_graphs = sorted(nx.connected_component_subgraphs(tmp_g), key=len, reverse=True)
+    sorted_graphs = sorted(nx.connected_component_subgraphs(tmp_g), key=len,
+                           reverse=True)
     for i in range(1, len(sorted_graphs)):
         for node in sorted_graphs[i].nodes():
             if verbose:
@@ -91,7 +93,8 @@ def download_kegg_pathway(pathway, overwrite=True):
         os.mkdir("KEGG")
     if os.path.exists(os.path.join("KEGG", '%s.xml' % pathway)):
         if overwrite:
-            warnings.warn('Removing pathway to download new one, can change behavior if you set "overwrite"=False')
+            warnings.warn('Removing pathway to download new one, '
+                          'can change behavior if you set "overwrite"=False')
         else:
             print("%s already downloaded" % pathway)
             download = False
@@ -104,7 +107,8 @@ def download_kegg_pathway(pathway, overwrite=True):
                 f.write(file_name)
 
 
-def build_network(gene_list, num_overlap=1, save_name='tmp', species='hsa', overwrite=True):
+def build_network(gene_list, num_overlap=1, save_name='tmp', species='hsa',
+                  overwrite=True):
     """ Construct a network from a list of gene names.
     build_network_kegg_ids(gene_list, num_overlap ,save_name )
     :param gene_list: List of genes to construct network
@@ -116,13 +120,15 @@ def build_network(gene_list, num_overlap=1, save_name='tmp', species='hsa', over
     """
 
     end_network = nx.DiGraph()
-    list_of_all = find_kegg_pathways(protein_names=gene_list, num_overlap=num_overlap, species=species,
+    list_of_all = find_kegg_pathways(protein_names=gene_list,
+                                     num_overlap=num_overlap, species=species,
                                      overwrite=overwrite)
     list_of_graphs = []
     networks_added = []
     for each in list_of_all:
         networks_added.append(each)
-        tmp, pathways_to_add, compounds = kgml_to_graph("%s.xml" % each, species=species)
+        tmp, pathways_to_add, compounds = kgml_to_graph("%s.xml" % each,
+                                                        species=species)
         list_of_graphs.append(tmp)
     for tmp in list_of_graphs:
         for nd in tmp.nodes():
@@ -147,3 +153,63 @@ def build_network(gene_list, num_overlap=1, save_name='tmp', species='hsa', over
     nx.nx.nx_agraph.write_dot(end_network, '%s.dot' % save_name)
 
     return end_network
+
+
+import cPickle as pickle
+
+directory = '/home/pinojc/PycharmProjects/Magine/Mappings'
+human_kegg_to_uniprot = pickle.load(
+        open(os.path.join(directory, 'human_kegg_mapper.p'), 'rb'))
+
+
+def download_all_of_kegg(output_dir):
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+    kegg.organism = 'hsa'
+    list_of_kegg_pathways = kegg.pathwayIds
+    print("Number of pathways  = {}".format(len(list_of_kegg_pathways)))
+    for i in list_of_kegg_pathways:
+        pathway = i[5:]
+        file_name = kegg.get(pathway, "kgml")
+        if file_name == 404:
+            print("%s ended with 404 error" % pathway)
+        else:
+            with open(os.path.join(output_dir, '%s.xml' % pathway), 'w') as f:
+                f.write(file_name)
+
+        graph, pathway_name, _ = kgml_to_graph('%s.xml' % pathway,
+                                               output_dir,
+                                               species='hsa')
+        print('{} has {} nodes and {} edges'.format(pathway_name,
+                                                    len(graph.nodes()),
+                                                    len(graph.edges())))
+        output_name = os.path.join(output_dir, '{}.gml'.format(pathway))
+        nx.write_gml(graph, output_name)
+
+
+def create_all_of_kegg(output_dir):
+    kegg.organism = 'hsa'
+    from Mappings.human_mapper import GeneMapper
+    gm = GeneMapper()
+    list_of_kegg_pathways = kegg.pathwayIds
+    all_of_kegg = nx.DiGraph()
+    species_not_in_dict = set()
+    for i in list_of_kegg_pathways:
+        pathway = i[5:]
+        output_name = os.path.join(output_dir, '{}.gml'.format(pathway))
+        graph = nx.read_gml(output_name)
+        all_of_kegg = nx.compose(all_of_kegg, graph)
+        for j in graph.nodes():
+            if not j.startswith('hsa'):
+                continue
+            if j in gm.kegg_to_genename:
+
+                continue
+            else:
+                species_not_in_dict.add(j)
+
+    print(len(species_not_in_dict))
+    print('{} has {} nodes and {} edges'.format("all of kegg",
+                                                len(all_of_kegg.nodes()),
+                                                len(all_of_kegg.edges())))
+    nx.write_gml(all_of_kegg, 'all_of_kegg.gml')
