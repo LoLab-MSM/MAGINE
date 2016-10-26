b19574807b7d5fa87ad91bc68ecc97cb954c07f2
jamespino <james.c.pino@vanderbilt.edu>
2016-10-26 17:31:40 -0500

    Added tmpfile use for building HMDB database. Cleaned up chemical_mapper, added reload function to reinit the class if you redowloaded HMDB.

--- Mappings/HMDB_processing/hmdb.py
+++ Mappings/HMDB_processing/hmdb.py
@@ -1,109 +1,145 @@
+"""
+HMDB database download and processing
+"""
 import czipfile as zipfile
 import os
 
+import numpy as np
 import pandas as pd
+import tempfile
 import urllib2
+import xml.etree.cElementTree as ElementTree
 
-from Mappings.xml_to_dictionary import ElementTree, XmlDictConfig, \
-    XmlListConfig
+from Mappings.xml_to_dictionary import XmlDictConfig, XmlListConfig
 
 directory = os.path.dirname(__file__)
 
 
-def download_hmdb():
-    hmdb_db_url = 'http://www.hmdb.ca/system/downloads/current/hmdb_metabolites.zip'
-    file_name = hmdb_db_url.split('/')[-1]
-    u = urllib2.urlopen(hmdb_db_url)
-    out_path = os.path.join(directory, file_name)
-    f = open(out_path, 'wb')
-    meta = u.info()
-    file_size = int(meta.getheaders("Content-Length")[0])
-    print("Downloading: %s Bytes: %s" % (file_name, file_size))
-    file_size_dl = 0
-    block_sz = 8192
-    while True:
-        buffer = u.read(block_sz)
-        if not buffer:
-            break
-        file_size_dl += len(buffer)
-        f.write(buffer)
-        status = r"%10d  [%3.2f%%]" % (
-        file_size_dl, file_size_dl * 100. / file_size)
-        status = status + chr(8) * (len(status) + 1)
-        print(status)
-    f.close()
+class HMDB:
+    """ Downloads and processes HMDB metabolite database
 
+    """
 
-def parse_hmdb():
-    out_dir = os.path.join(directory, 'HMDB')
+    def __init__(self):
+        self.tmp_dir = tempfile.mkdtemp()
+        self.out_dir = directory
+        self.target_file = 'hmdb_metabolites.zip'
 
-    def unzip_hmdb():
+    def download_hmdb(self):
+        """ Downloads hmdb metabolite xml file
 
-        hmdb_file = os.path.join(directory, 'hmdb_metabolites.zip')
+        :return:
+        """
+
+        hmdb_db_url = 'http://www.hmdb.ca/system/downloads/current/hmdb_metabolites.zip'
+        u = urllib2.urlopen(hmdb_db_url)
+        out_path = os.path.join(self.tmp_dir, self.target_file)
+        f = open(out_path, 'wb')
+        meta = u.info()
+        file_size = int(meta.getheaders("Content-Length")[0])
+        print("Downloading: %s Bytes: %s" % (self.target_file, file_size))
+        file_size_dl = 0
+        block_sz = 8192
+        v = set()
+        while True:
+            buffer = u.read(block_sz)
+            if not buffer:
+                break
+            file_size_dl += len(buffer)
+            f.write(buffer)
+            percent_download = np.floor(file_size_dl * 100. / file_size)
+            if percent_download % 10 == 0:
+                status = r"%10d  [%3.2f%%]" % (
+                    file_size_dl, file_size_dl * 100. / file_size)
+                status += chr(8) * (len(status) + 1)
+                if percent_download not in v:
+                    print(status)
+                    v.add(percent_download)
+        f.close()
+        print("Downloaded {} and stored {}".format(hmdb_db_url, out_path))
+
+    def unzip_hmdb(self, out_directory):
+        """ unzips hmdb metabolite file
+
+        :return:
+        """
+        hmdb_file = os.path.join(self.tmp_dir, self.target_file)
         if not os.path.exists(hmdb_file):
-            download_hmdb()
-        if not os.path.exists(out_dir):
-            os.mkdir(out_dir)
+            print("Downloading metabolite information from HMDB")
+            self.download_hmdb()
+        print("Unzipping metabolite file")
         zip_ref = zipfile.ZipFile(hmdb_file, 'r')
-        zip_ref.extractall(out_dir)
+        zip_ref.extractall(out_directory)
         zip_ref.close()
 
-    count = 0
-    categories = ['kegg_id', 'name', 'accession', 'chebi_id', 'chemspider_id',
-                  'biocyc_id', 'synonyms', 'pubchem_compound_id',
-                  'protein_associations',
-                  # 'secondary_accessions',
-                  # 'iupac_name',
-                  # 'normal_concentrations','chemical_formula', 'smiles',
-                  # 'drugbank_id', 'average_molecular_weight',
-                  # 'pathways', 'metlin_id',
-                  # 'inchikey',
-                  ]
+    def parse_hmdb(self):
+        """ parse HMDB to Pandas.DataFrame
 
-    tmp_all = []
-    for i in os.listdir(out_dir):
-        if i.startswith('H'):
-            # if count > 100:
-            #     continue
-            count += 1
-            filename = os.path.join(out_dir, i)
-            tree = ElementTree.parse(filename)
-            root = tree.getroot()
-            xmldict = XmlDictConfig(root)
-            tmp = []
-            for cat in categories:
-                info = xmldict[cat]
-                if info is None:
-                    tmp.append(info)
-                elif cat == 'protein_associations':
-                    tmp_list = []
-                    if type(info) == str:
-                        continue
-                    else:
-                        if type(info['protein']) == XmlDictConfig:
-                            tmp_list.append(info['protein']['gene_name'])
+        """
+        out_dir = os.path.join(self.tmp_dir, 'HMDB')
+        if not os.path.exists(out_dir):
+            os.mkdir(out_dir)
+            self.unzip_hmdb(out_dir)
 
-                        elif type(info['protein']) == XmlListConfig:
-                            for ii in info['protein']:
-                                tmp_list.append(ii['gene_name'])
-                    tmp.append(tmp_list)
-                elif cat == 'synonyms':
-                    tmp_list = []
-                    if type(info) == str:
-                        continue
-                    else:
-                        if type(info['synonym']) == str:
-                            tmp_list.append(info['synonym'])
+        count = 0
+        categories = ['kegg_id', 'name', 'accession', 'chebi_id',
+                      'chemspider_id',
+                      'biocyc_id', 'synonyms', 'pubchem_compound_id',
+                      'protein_associations',
+                      # 'secondary_accessions',
+                      # 'iupac_name',
+                      # 'normal_concentrations','chemical_formula', 'smiles',
+                      # 'drugbank_id', 'average_molecular_weight',
+                      # 'pathways', 'metlin_id',
+                      'inchikey',
+                      ]
+        print("Parsing metabolite information from files")
+        tmp_all = []
+        for i in os.listdir(out_dir):
+            if i.startswith('H'):
+                # if count > 100:
+                #     continue
+                count += 1
+                filename = os.path.join(out_dir, i)
+                tree = ElementTree.parse(filename)
+                xmldict = XmlDictConfig(tree.getroot())
+                tmp = []
+                for cat in categories:
+                    info = xmldict[cat]
+                    if info is None:
+                        tmp.append(info)
+                    elif cat == 'protein_associations':
+                        tmp_list = []
+                        if type(info) == str:
+                            continue
                         else:
-                            for each in info['synonym']:
-                                tmp_list.append(each)
-                    tmp.append(tmp_list)
-                else:
-                    tmp.append(info.encode('ascii', 'ignore'))
-            tmp_all.append(tmp)
-    df = pd.DataFrame(tmp_all, columns=categories)
-    df.to_csv(os.path.join(directory, 'hmdb_dataframe.csv.gz'),
-              compression='gzip')
+                            if type(info['protein']) == XmlDictConfig:
+                                tmp_list.append(info['protein']['gene_name'])
+
+                            elif type(info['protein']) == XmlListConfig:
+                                for ii in info['protein']:
+                                    tmp_list.append(ii['gene_name'])
+                        tmp.append(tmp_list)
+                    elif cat == 'synonyms':
+                        tmp_list = []
+                        if type(info) == str:
+                            continue
+                        else:
+                            if type(info['synonym']) == str:
+                                tmp_list.append(info['synonym'])
+                            else:
+                                for each in info['synonym']:
+                                    tmp_list.append(each)
+                        tmp.append(tmp_list)
+                    else:
+                        tmp.append(info.encode('ascii', 'ignore'))
+                tmp_all.append(tmp)
+        df = pd.DataFrame(tmp_all, columns=categories)
+        df.to_csv(os.path.join(self.out_dir, 'hmdb_dataframe.csv.gz'),
+                  compression='gzip')
+        print("Done processing HMDB")
 
 
-parse_hmdb()
+if __name__ == '__main__':
+    hm = HMDB()
+    hm.parse_hmdb()
--- Mappings/chemical_mapper.py
+++ Mappings/chemical_mapper.py
@@ -11,28 +11,42 @@ class ChemicalMapper:
     converts chemical species
     Datbase was creating by pulling down everything from HMDB
     """
-    categories = ['kegg_id', 'name', 'accession', 'chebi_id', 'chemspider_id', 'biocyc_id', 'synonyms',
+    categories = ['kegg_id', 'name', 'accession', 'chebi_id', 'chemspider_id',
+                  'biocyc_id', 'synonyms',
                   'pubchem_compound_id', 'iupac_name', 'protein_associations']
 
     def __init__(self):
+        self.database = None
+        self.hmdb_accession_to_chemical_name = {}
+        self.chemical_name_to_hmdb_accession = {}
+        self.hmdb_accession_to_kegg = {}
+        self.kegg_to_hmdb_accession = {}
+        self.hmdb_accession_to_protein = {}
+        self.synonyms_to_hmdb = {}
         try:
             self.load()
+            print('Loading class data')
         except:
+            print('Initializing Chemical mapping')
+            self.reload()
+
+    def reload(self):
             hmdb_database = pd.read_csv(
-                os.path.join(directory, 'HMDB_processing',
-                             'hmdb_dataframe.csv.gz'))
+                    os.path.join(directory, 'HMDB_processing',
+                                 'hmdb_dataframe.csv.gz'))
             self.database = hmdb_database
             self.hmdb_accession_to_chemical_name = self.convert_to_dict(
-                "accession", "name")
-            self.chemical_name_to_hmdb_accession = self.convert_to_dict("name",
-                                                                        "accession")
+                    "accession", "name")
+            self.chemical_name_to_hmdb_accession = self.convert_to_dict(
+                    "name", "accession")
             self.hmdb_accession_to_kegg = self.convert_to_dict("accession",
                                                                "kegg_id")
             self.kegg_to_hmdb_accession = self.convert_to_dict("kegg_id",
                                                                "accession")
-            self.hmdb_accession_to_protein = self.convert_to_dict("accession",
-                                                                  "protein_associations")
+            self.hmdb_accession_to_protein = self.convert_to_dict(
+                    "accession", "protein_associations")
             self.synonyms_to_hmdb = None
+            self.save()
 
     def convert_to_dict(self, key, value):
         """ creates a dictionary from hmdb with a list of values for each key
@@ -63,9 +77,12 @@ class ChemicalMapper:
         :return:
         """
 
-        print('Number of HMDB accessions = {0}'.format(len(self.database['accession'].unique())))
-        print('Number of unique KEGG ids = {0}'.format(len(self.hmdb_accession_to_kegg.keys())))
-        print('Number of HMDB to KEGG mappings = {0}'.format(len(self.kegg_to_hmdb_accession.values())))
+        print('Number of HMDB accessions = {0}'.format(
+                len(self.database['accession'].unique())))
+        print('Number of unique KEGG ids = {0}'.format(
+                len(self.hmdb_accession_to_kegg.keys())))
+        print('Number of HMDB to KEGG mappings = {0}'.format(
+                len(self.kegg_to_hmdb_accession.values())))
 
     def save(self):
         """ save class instance
@@ -82,8 +99,12 @@ class ChemicalMapper:
         :return:
         """
 
-        print('Loading class data')
         with open(os.path.join(directory, 'hmdb_instance.txt'), 'r') as f:
             dataPickle = f.read()
             f.close()
         self.__dict__ = cPickle.loads(dataPickle)
+
+
+if __name__ == '__main__':
+    cm = ChemicalMapper()
+    cm.print_info()
