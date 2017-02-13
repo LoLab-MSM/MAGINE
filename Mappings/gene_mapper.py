3bef7f4bae85ce3de372d809d340664d1672f3d2
James Pino <james.c.pino@vanderbilt.edu>
2017-02-13 13:51:31 -0600

    Updating to use cpickle if python3.

--- Mappings/gene_mapper.py
+++ Mappings/gene_mapper.py
@@ -1,4 +1,7 @@
-import cPickle as pickle
+try:
+    import cPickle as pickle
+except:  # python3 doesnt have cPickle
+    import pickle
 import os
 
 import pandas as pd
@@ -12,6 +15,16 @@ mouse_uniprot_to_gene_name = pickle.load(open(os.path.join(directory, 'mouse_uni
 human_uniprot_to_gene_name = pickle.load(open(os.path.join(directory, 'network_based_UPids_to_genes.p'), 'rb'))
 human_uniprot_to_gene_name_2 = pickle.load(open(os.path.join(directory, 'kegg_to_uniprot_gene_name.p'), 'rb'))
 human_kegg_to_uniprot = pickle.load(open(os.path.join(directory, 'human_kegg_mapper.p'), 'rb'))
+
+
+def convert_to_dict(data, key, value):
+    """ creates a dictionary with a list of values for each key
+
+    :param key:
+    :param value:
+    :return:
+    """
+    return {k: list(v) for k, v in data.groupby(key)[value]}
 
 
 class GeneMapper:
@@ -29,25 +42,20 @@ class GeneMapper:
         hgnc = pd.read_csv(os.path.join(directory, 'data', 'hngc.gz'))
         self.database = hgnc
         # hgnc
-        self.gene_name_to_uniprot = self.convert_to_dict(hgnc, 'symbol', 'uniprot_ids')
-        self.gene_name_to_alias_name = self.convert_to_dict(hgnc, 'symbol', 'alias_name')
-        self.gene_name_to_ensembl = self.convert_to_dict(hgnc, 'symbol', 'ensembl_gene_id')
+        self.gene_name_to_uniprot = convert_to_dict(hgnc, 'symbol',
+                                                    'uniprot_ids')
+        self.gene_name_to_alias_name = convert_to_dict(hgnc, 'symbol',
+                                                       'alias_name')
+        self.gene_name_to_ensembl = convert_to_dict(hgnc, 'symbol',
+                                                    'ensembl_gene_id')
         self.gene_name_to_kegg = None
 
-        self.uniprot_to_gene_name = self.convert_to_dict(hgnc, 'uniprot_ids', 'symbol')
+        self.uniprot_to_gene_name = convert_to_dict(hgnc, 'uniprot_ids',
+                                                    'symbol')
         self.uniprot_to_kegg = None
         self.protein_name_to_gene_name = None
         self.protein_name_to_uniprot = None
         self.kegg_to_uniprot = None
-
-    def convert_to_dict(self, data, key, value):
-        """ creates a dictionary with a list of values for each key
-
-        :param key:
-        :param value:
-        :return:
-        """
-        return {k: list(v) for k, v in data.groupby(key)[value]}
 
     def check_synonym_dict(self, term, format_name):
         """ checks hmdb database for synonyms and returns formatted name
