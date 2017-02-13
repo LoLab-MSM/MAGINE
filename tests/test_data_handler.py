34716c04e7e3d214c44670d1336e018ce46b1e9a
James Pino <james.c.pino@vanderbilt.edu>
2017-02-13 13:59:14 -0600

    Adding tests of plotting with multiprocessing to tests.

--- tests/test_data_handler.py
+++ tests/test_data_handler.py
@@ -1,8 +1,12 @@
 import os
+import sys
 
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import pathos.multiprocessing as mp
 from magine.data_handler import ExperimentalData
 
 data_directory = os.path.join(os.path.dirname(__file__), 'Data')
+# data_directory = os.path.join(os.path.dirname(__file__), 'magine')
 exp_data = ExperimentalData('example_apoptosis.csv', data_directory)
 
 
@@ -15,4 +19,37 @@ def test_time_series_volcano():
                                  bh_critera=True)
 
 
-test_time_series_volcano()
+def test_plot_list():
+    l = ['ADORA1', 'PARP1', 'BAX']
+    x = list(exp_data.list_proteins)
+    processes = []
+    import time
+    st = time.time()
+    for i in x:
+        # p = mp.Process(target=exp_data.plot_list_of_genes,
+        #                args=(list(x), i, 'proteins', i, False, True))
+        processes.append((list(x), 'out', 'proteins', i, False, True))
+        # processes.append(p)
+        # p.start()
+        # exp_data.plot_list_of_genes(list_of_genes=list(x), save_name=i,
+        #                             out_dir='proteins', title=i,
+        #                             plot_all_x=False, log_scale=False)
+    # [x.start() for x in processes]
+    # """
+    pool = mp.Pool(4)
+    # for task in processes:
+    #     pool.apply(exp_data.plot_list_of_genes, args=(task,))
+    pool.map(exp_data.plot_list_of_genes, processes)
+    pool.close()
+    pool.join()
+    # """
+    print(time.time() - st)
+
+
+def test_html_output():
+    exp_data.plot_all_proteins()
+
+
+# cProfile.run('test_plot_list()', sort=1)
+if __name__ == '__main__':
+    test_plot_list()
