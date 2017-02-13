8124bfd89af3cc9c9f897befb000e72d74846727
James Pino <james.c.pino@vanderbilt.edu>
2017-02-13 13:56:11 -0600

    Adding test with all_measured_list (reactome expansion flag)

--- tests/test_network_generation.py
+++ tests/test_network_generation.py
@@ -1,13 +1,14 @@
-from magine.networks.network_generator import build_network, create_all_of_kegg
+from magine.networks.network_generator import build_network
 
 
 def test_build_network():
     graph = build_network(['Bax', 'tp53', 'JAK1', 'bad'], num_overlap=1, save_name='tmp', species='hsa',
-                          overwrite=False)
+                          overwrite=False, all_measured_list=['casp3', 'egfr'])
     for i in graph.nodes():
         if len(i.split(':')) > 1:
             print(i)
 
 
 # download_all_of_kegg('test')
-create_all_of_kegg('test')
+if __name__ == '__main__':
+    test_build_network()
