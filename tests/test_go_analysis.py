4c67b66a7b3bceea900ffe61985f0ce61678f66f
James Pino <james.c.pino@vanderbilt.edu>
2017-02-24 18:06:36 -0600

    Removing tests that were for the older version of magine.

--- tests/test_go_analysis.py
+++ tests/test_go_analysis.py
@@ -2,61 +2,15 @@ from magine.ontology.ontology_analysis import GoAnalysis
 from sample_experimental_data import exp_data
 
 
-
-def test_go_heatmap():
-    """
-    tests the ability to create a heatmap from list of genes
-    :return None
-    """
-    go = GoAnalysis(species='hsa', output_directory='tmp')
-    go.analyze_data([['BAX', 'BAK1', 'BOK'],
-                     ['BCL2L1', 'BCL2', 'MCL1'], ],
-                    # ['BAD', 'BID', 'BBC3'],
-                      #['BAX', 'BAK1', 'BOK', 'BCL2L1', 'BCL2','MCL1', 'BAD', 'BID', 'BBC3']],
-                    labels=['1', '2'], savename='test_11', )
-    go.export_to_html(labels=[0, 1, ], html_name='test_out1')
-
-
-def test_print_hit_by_index():
-    """
-    tests the ability to create a heatmap from list of genes
-    :return None
-    """
-    go = GoAnalysis(species='hsa', output_directory='tmp', verbose=True)
-    go.analyze_data([
-        ['BAX', 'BAK1', 'BOK', 'BCL2L1', 'BCL2', 'MCL1', 'BAD', 'BID', 'BBC3']],
-            labels=['1', '2'], savename='test_11')
-    go.retrieve_top_ranked(0, number=10)
-
-
-def test_print_top_hits():
-    """
-    tests the ability to create a heatmap from list of genes
-    :return None
-    """
-    go = GoAnalysis(species='hsa', output_directory='tmp2', verbose=False,)
-    # go.analyze_data([['ATR', 'CHEK1', 'CHEK2', 'ATM', 'TP53', 'BAX', 'CYCS', 'CASP3', 'CASP8','FAS']],
-    #                 aspect='P', labels=['1'], savename='test_11', analyze=False)
-
-    go.analyze_data([['BAX', 'BAK1', 'BOK',
-                       'BCL2L1', 'BCL2', 'MCL1',
-                       'BAX', 'BAK1', 'BOK', 'BCL2L1', 'BCL2', 'MCL1', 'BAD',
-                       'BID', 'BBC3']],
-                    labels=['1', '2'], savename='test_11',
-                    )
-    go.export_to_html(labels=['1'], html_name='test')
-    go.print_ranked_over_time(savename='yolo', labels=None, number=9,
-                              create_plots=True)
-
-
 def test_html():
     go = GoAnalysis(species='hsa', output_directory='html_output',
                     verbose=False,
                     experimental_data=exp_data)
 
-    df = go.create_enrichment_array(exp_data.proteomics_up_over_time[:2],
-                                    exp_data.timepoints[:2],
-                                    save_name='test_df')
+    df = go.create_enrichment_array(exp_data.proteomics_up_over_time,
+                                    exp_data.timepoints,
+                                    save_name='test_enrichment_array')
+    quit()
     go.write_table_to_html(save_name='index')
 
     go.write_table_to_html(df, save_name='index2')
@@ -64,5 +18,3 @@ def test_html():
 
 if __name__ == '__main__':
     test_html()
-#test_print_top_hits()
-    # test_print_top_hits()
