124409408c122a739a7333ad9efde3e88b6c10ac
James Pino <james.c.pino@vanderbilt.edu>
2017-02-24 18:04:13 -0600

    Adding larger sample of experimental data

--- tests/sample_experimental_data.py
+++ tests/sample_experimental_data.py
@@ -3,5 +3,5 @@ import os
 from magine.data_handler import ExperimentalData
 
 data_dir = os.path.dirname(__file__)
-exp_data = ExperimentalData(proteomics_file='example_apoptosis.csv',
+exp_data = ExperimentalData(proteomics_file='large_example.csv',
                             data_directory=os.path.join(data_dir, 'Data'))
