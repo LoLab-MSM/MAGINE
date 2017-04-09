53a9ba4ea70db11f3087b0b061f9321b05aad2e8
James Pino <james.c.pino@vanderbilt.edu>
2017-04-09 18:20:27 -0500

    Adding auto download and setup if file does not exist. Now reactome functional interaction is downloaded the same as hdmb

--- magine/network_database_expansions/expansion_network.py
+++ magine/network_database_expansions/expansion_network.py
@@ -1,9 +1,19 @@
 import networkx as nx
 import os
+from reactome_expansion.download_reactome_functional_interaction import ReactomeFunctionalInteraction
 
 
 def load_reactome_fi():
     path = os.path.join(os.path.dirname(__file__),
                         'reactome_expansion',
                         'reactome_fi.gml')
+    if not os.path.exists(path):
+        print("Downloading Reactome Functional interaction network!")
+        rfi = ReactomeFunctionalInteraction()
+        rfi.setup()
+        rfi.parse_network()
+    if not os.path.exists(path):
+        print('Failed!')
+        quit()
     return nx.read_gml(path)
+load_reactome_fi()
--- magine/network_database_expansions/reactome_expansion/download_reactome_functional_interaction.py
+++ magine/network_database_expansions/reactome_expansion/download_reactome_functional_interaction.py
@@ -1,138 +1,153 @@
 import networkx as nx
 import pandas as pd
+import os
+import tempfile
+import requests
+import numpy as np
 
-url = 'http://reactomews.oicr.on.ca:8080/caBigR3WebApp2015/FIsInGene_031516_with_annotations.txt.zip'
-count = 0
-directions = set()
-genes = set()
-annotation = set()
-
-
-def return_direction(text, gene1, gene2):
-    if text in ("<-", "?-"):
-        return gene2, gene1
-    if text in ("->", "->"):
-        return gene1, gene2
-    else:
-        return gene1, gene2
+directory = os.path.dirname(__file__)
 
 
-table = pd.read_table('FIsInGene_031516_with_annotations.txt')
+class ReactomeFunctionalInteraction(object):
+    def __init__(self):
+        self.tmp_dir = tempfile.mkdtemp()
+        self.out_dir = directory
+        self.target_file = 'FIsInGene_031516_with_annotations.txt.zip'
+        self.url = 'http://reactomews.oicr.on.ca:8080/caBigR3WebApp2015/FIsInGene_031516_with_annotations.txt.zip'
+        self.out_path = os.path.join(self.tmp_dir, self.target_file)
+
+    def setup(self):
+        r = requests.get(self.url, stream=True)
+        response = requests.head(self.url)
+        file_size = int(response.headers['content-length'])
+        print("Downloading: %s Bytes: %s" % (self.target_file, file_size))
+        file_size_dl = 0
+        block_sz = 8192
+        v = set()
+        milestone_markers = range(0, 101, 10)
+
+        with open(self.out_path, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=block_sz):
+                file_size_dl += len(chunk)
+                percent_download = int(
+                    np.floor(file_size_dl * 100. / file_size))
+
+                if percent_download in milestone_markers:
+                    if percent_download not in v:
+                        print("{}%".format(percent_download))
+                        v.add(percent_download)
+                if chunk:  # filter out keep-alive new chunks
+                    f.write(chunk)
+
+        print("Downloaded {} and stored {}".format(self.url, self.out_path))
+
+    def parse_network(self):
+        table = pd.read_table(self.out_path)
+        g = nx.DiGraph()
+        added_genes = set()
+        for i, row in table.iterrows():
+            score = row['Score']
+            gene1, gene2, interaction, prediction, mod = self.check_row(row)
+            if gene1 == gene2:
+                continue
+            elif prediction:
+                if gene1 not in added_genes:
+                    g.add_node(gene1, sourceDB='ReactomeFI', speciesType='gene')
+                    added_genes.add(gene1)
+                if gene2 not in added_genes:
+                    g.add_node(gene2, sourceDB='ReactomeFI', speciesType='gene')
+                    added_genes.add(gene2)
+                g.add_edge(gene1, gene2, interactionType=interaction, ptm=mod,
+                           score=score, prediction='True', sourceDB='ReactomeFI')
+            else:
+                if gene1 not in added_genes:
+                    g.add_node(gene1, sourceDB='ReactomeFI', speciesType='gene')
+                    added_genes.add(gene1)
+                if gene2 not in added_genes:
+                    g.add_node(gene2, sourceDB='ReactomeFI', speciesType='gene')
+                    added_genes.add(gene2)
+
+                g.add_edge(gene1, gene2, interactionType=interaction, ptm=mod,
+                           score=score, sourceDB='ReactomeFI')
+        ge = set(g.nodes())
+        g1 = set(table['Gene1'])
+        g2 = set(table['Gene2'])
+        g_all = set()
+        g_all.update(g1)
+        g_all.update(g2)
+        for i in g_all:
+            if i not in ge:
+                print(i)
+
+        print("Reactome network has {} nodes ".format(len(g.nodes())))
+        print("Reactome network has {} edges ".format(len(g.edges())))
+        nx.write_gml(g, os.path.join(directory, 'reactome_fi.gml'))
+
+    @staticmethod
+    def return_direction(text, gene1, gene2):
+        if text in ("<-", "?-"):
+            return gene2, gene1
+        if text in ("->", "->"):
+            return gene1, gene2
+        else:
+            return gene1, gene2
 
 
-def check_row(row):
-    prediction = False
-    interaction_type = None
-    annotation = row['Annotation']
-    modification = ''
-    if 'predicted' in annotation:
-        prediction = True
-    if 'repress' in annotation:
-        interaction_type = 'repression'
-    elif 'express' in annotation:
-        interaction_type = 'expression'
-    elif 'reaction' in annotation:
-        interaction_type = 'reaction'
-    elif 'complex' in annotation:
-        interaction_type = 'complex'
-    elif 'catalyze' in annotation:
-        interaction_type = 'catalyze'
-    elif 'deactiv' in annotation:
-        interaction_type = 'deactivate'
-    elif 'activ' in annotation:
-        interaction_type = 'activate'
-    elif 'inhib' in annotation:
-        interaction_type = 'inhibit'
-    elif 'binding' in annotation:
-        interaction_type = 'binding'
-    elif 'input' in annotation:
-        interaction_type = '?'
-    elif 'indirect effect' in annotation:
-        interaction_type = 'indirect'
-    elif 'compound' in annotation:
-        interaction_type = 'compound'
-    elif 'state change' in annotation:
-        interaction_type = 'state change'
-    elif 'interaction' in annotation:
-        interaction_type = 'binding'
-    elif 'PPrel' in annotation:
-        interaction_type = 'binding'
-    if 'dephosphoryl' in annotation:
-        modification = 'dephosphorylation'
+    def check_row(self, row):
+        prediction = False
+        interaction_type = None
+        annotation = row['Annotation']
+        modification = ''
+        if 'predicted' in annotation:
+            prediction = True
+        if 'repress' in annotation:
+            interaction_type = 'repression'
+        elif 'express' in annotation:
+            interaction_type = 'expression'
+        elif 'reaction' in annotation:
+            interaction_type = 'reaction'
+        elif 'complex' in annotation:
+            interaction_type = 'complex'
+        elif 'catalyze' in annotation:
+            interaction_type = 'catalyze'
+        elif 'deactiv' in annotation:
+            interaction_type = 'deactivate'
+        elif 'activ' in annotation:
+            interaction_type = 'activate'
+        elif 'inhib' in annotation:
+            interaction_type = 'inhibit'
+        elif 'binding' in annotation:
+            interaction_type = 'binding'
+        elif 'input' in annotation:
+            interaction_type = '?'
+        elif 'indirect effect' in annotation:
+            interaction_type = 'indirect'
+        elif 'compound' in annotation:
+            interaction_type = 'compound'
+        elif 'state change' in annotation:
+            interaction_type = 'state change'
+        elif 'interaction' in annotation:
+            interaction_type = 'binding'
+        elif 'PPrel' in annotation:
+            interaction_type = 'binding'
+        if 'dephosphoryl' in annotation:
+            modification = 'dephosphorylation'
+            if interaction_type is None:
+                interaction_type = 'dephosphorylation'
+        elif 'phosphoryl' in annotation:
+            modification = 'phosphorylation'
+            if interaction_type is None:
+                interaction_type = 'phosphorylation'
+        elif 'ubiquiti' in annotation:
+            modification = 'ubiquitination'
+            if interaction_type is None:
+                interaction_type = 'ubiquitination'
         if interaction_type is None:
-            interaction_type = 'dephosphorylation'
-    elif 'phosphoryl' in annotation:
-        modification = 'phosphorylation'
-        if interaction_type is None:
-            interaction_type = 'phosphorylation'
-    elif 'ubiquiti' in annotation:
-        modification = 'ubiquitination'
-        if interaction_type is None:
-            interaction_type = 'ubiquitination'
-    if interaction_type is None:
-        print(annotation)
-        interaction_type = '?'
-        if 'ECrel' in annotation:
-            print(row)
-    g1, g2 = return_direction(row['Direction'], row['Gene1'], row['Gene2'])
-    return g1, g2, interaction_type, prediction, modification
+            interaction_type = '?'
+        g1, g2 = self.return_direction(row['Direction'], row['Gene1'], row['Gene2'])
+        return g1, g2, interaction_type, prediction, modification
 
-
-g = nx.DiGraph()
-added_genes = set()
-for i, row in table.iterrows():
-    score = row['Score']
-    gene1, gene2, interaction, prediction, modification = check_row(row)
-    if prediction:
-        if gene1 not in added_genes:
-            g.add_node(gene1, sourceDB='ReactomeFI', speciesType='gene')
-            added_genes.add(gene1)
-        if gene2 not in added_genes:
-            g.add_node(gene2, sourceDB='ReactomeFI', speciesType='gene')
-            added_genes.add(gene2)
-        g.add_edge(gene1, gene2, interactionType=interaction, ptm=modification,
-                   score=score, predection='true', sourceDB='ReactomeFI')
-        # print(gene1, gene2, interaction, prediction, modification, score)
-    elif gene1 == gene2:
-        continue
-    else:
-        if gene1 not in added_genes:
-            g.add_node(gene1, sourceDB='ReactomeFI', speciesType='gene')
-            added_genes.add(gene1)
-        if gene2 not in added_genes:
-            g.add_node(gene2, sourceDB='ReactomeFI', speciesType='gene')
-            added_genes.add(gene2)
-
-        g.add_edge(gene1, gene2, interactionType=interaction, ptm=modification,
-                   score=score, sourceDB='ReactomeFI')
-
-        # print(gene1, gene2, interaction, prediction)
-ge = set(g.nodes())
-g1 = set(table['Gene1'].unique())
-g2 = set(table['Gene2'].unique())
-g_all = set(list(g1) + list(g2))
-for i in g_all:
-    if i not in ge:
-        print(i)
-
-print(len(g.nodes()))
-print(len(g.edges()))
-nx.write_gml(g, 'reactome_fi.gml')
-quit()
-with open('FIsInGene_031516_with_annotations.txt', 'r') as f:
-    f.readline()
-    alist = [line.rstrip() for line in f]
-    for line in alist:
-        if count > 100:
-            continue
-        Gene1, Gene2, Annotation, Direction, Score = line.split('\t')
-        print(Gene1, Gene2, Annotation, Direction, Score)
-        directions.add(Direction)
-        annotation.add(Annotation)
-        genes.add(Gene1)
-        genes.add(Gene2)
-        count += 1
-for each in annotation:
-    print(each)
-for i in [directions, genes, annotation]:
-    print(len(i))
+if __name__ == '__main__':
+    rfi = ReactomeFunctionalInteraction()
+    rfi.setup()
+    rfi.parse_network()
