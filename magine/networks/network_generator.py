d13f0f622a0c2cee9f380928d9d21b8d35b2e482
James Pino <james.c.pino@vanderbilt.edu>
2017-04-09 18:23:51 -0500

    Updated doc strings. Added 'use_reactome' flag to build_network call.

--- magine/networks/network_generator.py
+++ magine/networks/network_generator.py
@@ -8,7 +8,7 @@ from sys import modules
 
 import networkx as nx
 from bioservices import KEGG
-
+import itertools
 import Mappings.maps as mapper
 from magine.network_database_expansions.expansion_network import \
     load_reactome_fi
@@ -95,7 +95,7 @@ def download_kegg_pathway(pathway, overwrite=True):
         os.mkdir("KEGG")
     if os.path.exists(os.path.join("KEGG", '%s.xml' % pathway)):
         if overwrite:
-            warnings.warn('Removing pathway to download new one, '
+            warnings.warn('Removing KEGG pathway to download new one, '
                           'can change behavior if you set "overwrite"=False')
         else:
             print("%s already downloaded" % pathway)
@@ -110,17 +110,37 @@ def download_kegg_pathway(pathway, overwrite=True):
 
 
 def build_network(gene_list, num_overlap=1, save_name='tmp', species='hsa',
-                  overwrite=True, all_measured_list=None):
-    """ Construct a network from a list of gene names.
-    build_network_kegg_ids(gene_list, num_overlap ,save_name )
-    :param gene_list: List of genes to construct network
-    :param overwrite:
-    :param species: organism type
-    :param save_name: Output filename, optional, default tmp
-    :param num_overlap: number of hits by protein to be added to
-        the network. optional, Default 1
+                  overwrite=True, all_measured_list=None, use_reactome=True,
+                  use_hmdb=False):
     """
+    Construct a network from a list of gene names.
 
+    Parameters
+    ----------
+    gene_list : list
+        list of genes to construct network
+    num_overlap : int
+        number of KEGG pathways that a species must be in to add pathway to
+        network
+    save_name : str
+        output name to save network. Will save one before and after ID
+        conversion
+    species : str
+        species of proteins ('hsa': human, 'mmu':murine)
+    overwrite : bool
+        remove old KEGG xml files and download new ones
+    all_measured_list : list
+        list of all species that should be considered in network
+    use_reactome : bool
+        use reactome functional interaction to expand network
+    use_hmdb : bool
+        use hmdb to expand network incorporating metabolites provided in
+        all_measured_list
+
+    Returns
+    -------
+    networkx.DiGraph
+    """
     end_network = nx.DiGraph()
     list_of_all = find_kegg_pathways(protein_names=gene_list,
                                      num_overlap=num_overlap, species=species,
@@ -150,9 +170,19 @@ def build_network(gene_list, num_overlap=1, save_name='tmp', species='hsa',
     end_network = mapper.convert_all(end_network, species=species)
     if all_measured_list is not None:
         end_network = add_reactome(end_network, all_measured_list)
+    elif use_reactome:
+        end_network = add_reactome(end_network, gene_list)
+    if use_hmdb:
+        print("warning: automatic integration currently in progress.\n"
+              "For now, please use "
+              "\nfrom magine.network_database_expansions.hmdb_expansion.expand_by_hmdb"
+              "\n and provide metabolite list seprately")
+        # from magine.network_database_expansions.hmdb_expansion.expand_by_hmdb import expand_by_hmdb
+        # end_network = expand_by_hmdb(end_network, )
+    print("Trimming network")
     delete_disconnected_network(end_network)
-    print('Number of edges', len(end_network.edges()))
-    print('Number of nodes', len(end_network.nodes()))
+    print('Number of edges {}'.format(len(end_network.edges())))
+    print('Number of nodes {}'.format(len(end_network.nodes())))
     nx.write_gml(end_network, '%s.gml' % save_name)
     nx.nx.nx_agraph.write_dot(end_network, '%s.dot' % save_name)
 
@@ -160,39 +190,95 @@ def build_network(gene_list, num_overlap=1, save_name='tmp', species='hsa',
 
 
 def add_reactome(network, measured_list):
+    """
+    add reactome functional interaction network to network
+    Parameters
+    ----------
+    network : nx.DiGraph
+
+    measured_list : list
+
+
+    Returns
+    -------
+
+    """
     combined_network = network.copy()
     existing_nodes = set(network.nodes())
+    existing_edges = set(network.edges())
+    measured_set = set(measured_list)
 
     fi_network = load_reactome_fi()
+    reactome_nodes = set(fi_network.nodes())
+
     added_nodes = 0
     measured_species_in_network = set()
+    nodes_to_check = set()
+    for i in measured_set:
+        if i not in existing_nodes:
+            if i in reactome_nodes:
+                added_nodes += 1
+
+    for i in reactome_nodes:
+        if i in measured_set:
+            nodes_to_check.add(i)
 
     for i in fi_network.nodes(data=True):
         if i[0] in existing_nodes:
             continue
         elif i in measured_list:
             measured_species_in_network.add(i)
-
+            nodes_to_check.add(i)
     print("Found {} nodes in REACTOME not in KEGG".format(added_nodes))
 
     added_edges = 0
-    kegg_edges = set(combined_network.edges())
+    for node_1, node_2 in itertools.combinations(nodes_to_check, 2):
+        if nx.has_path(fi_network, node_1, node_2):
+            for path in nx.all_shortest_paths(fi_network, node_1, node_2):
+                _add_edges_from_path(fi_network, combined_network, path)
+        if nx.has_path(fi_network, node_2, node_1):
+            for path in nx.all_shortest_paths(fi_network, node_2, node_1):
+                _add_edges_from_path(fi_network, combined_network, path)
+
     reactome_edges = fi_network.edges(data=True)
     for i, j, k in reactome_edges:
-        if (i, j) in kegg_edges:
+        if (i, j) in existing_edges:
             continue
-        else:
+        if i in measured_set or j in measured_set:
             combined_network.add_edge(i, j, k)
             added_edges += 1
     print("Added {} edges from REACTOME".format(added_edges))
+
+    print("Nodes before Reactome expansion "
+          "= {}, after = {}".format(len(existing_nodes),
+                                    len(combined_network.nodes())))
     return combined_network
 
 
-# import cPickle as pickle
+def _add_edges_from_path(network, graph, path):
+    """
+    Adds a path to a graph
 
-# directory = '/home/pinojc/PycharmProjects/Magine/Mappings'
-# human_kegg_to_uniprot = pickle.load(
-#         open(os.path.join(directory, 'human_kegg_mapper.p'), 'rb'))
+    Parameters
+    ----------
+    graph : networkx.DiGraph
+        graph to add paths
+    path : list_like
+        list of species that create a path
+
+    """
+    previous = None
+    for protein in list(path):
+        if previous is None:
+            previous = protein
+            continue
+        else:
+            graph.add_node(previous, **network.node[previous])
+            graph.add_node(protein, **network.node[protein])
+            graph.add_edge(previous, protein,
+                           **network.edge[previous][protein])
+            previous = protein
+
 
 
 def download_all_of_kegg(output_dir):
