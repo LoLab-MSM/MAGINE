4d811d6d9269e5c64fb3ad4d7fa53e91ab70c346
James Pino <james.c.pino@vanderbilt.edu>
2017-04-17 00:07:40 -0500

    Correcting path to data.

--- magine/tests/test_data_merge.py
+++ magine/tests/test_data_merge.py
@@ -1,14 +1,17 @@
 import pandas as pd
 from nose.tools import raises
-
+import os
 from magine.data.formatter import load_csv, load_directory
+
 
 pd.set_option('display.width', 10000)
 pd.set_option('max_colwidth', 100)
 
+full_path = os.path.join(os.path.dirname(__file__), 'Data', 'hilic')
+
 
 def test_load_directory():
-    df = load_directory('Data/hilic')
+    df = load_directory(full_path)
     pivot_df = pd.pivot_table(df, columns='time',
                               index=['compound', 'compound_id', 'name', ])
     assert pivot_df.shape == (2, 28)
@@ -20,9 +23,10 @@ def test_directory_doesnt_exist():
 
 
 def test_load_csv():
-    df = load_csv('Data/hilic', 'hilic_1.csv')
+    df = load_csv(full_path, 'hilic_1.csv')
 
     assert df.shape == (2, 22)
 
 
-test_directory_doesnt_exist()
+if __name__ == '__main__':
+    test_directory_doesnt_exist()
