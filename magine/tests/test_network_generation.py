77d2539f97cc7b13cb91bf93847d36125029d231
James Pino <james.c.pino@vanderbilt.edu>
2017-06-05 18:54:10 -0500

    Changing travis time back to 30 mins. Removing hmdb use for tests. It takes too long.

--- magine/tests/test_network_generation.py
+++ magine/tests/test_network_generation.py
@@ -5,7 +5,7 @@ def test_build_network():
     graph = build_network(['Bax', 'tp53', 'JAK1', 'bad'], num_overlap=1,
                           save_name='sample_network', species='hsa',
                           overwrite=False, all_measured_list=['casp3', 'egfr'],
-                          use_hmdb=True, use_reactome=True
+                          use_hmdb=False, use_reactome=True
                           )
     for i in graph.nodes():
         if len(i.split(':')) > 1:
