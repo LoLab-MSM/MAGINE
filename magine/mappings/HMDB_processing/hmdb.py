6d5e44ec54503fa3f5ba33693ad019c88b9c3ed0
James Pino <james.c.pino@vanderbilt.edu>
2017-06-06 13:59:03 -0500

    Shaving more time off of hmdb processing.

--- magine/mappings/HMDB_processing/hmdb.py
+++ magine/mappings/HMDB_processing/hmdb.py
@@ -11,11 +11,6 @@ import numpy as np
 import pandas as pd
 import requests
 
-from magine.mappings.HMDB_processing.xml_to_dictionary import XmlDictConfig, \
-    XmlListConfig
-
-# from xml_to_dictionary import XmlDictConfig, XmlListConfig
-
 directory = os.path.dirname(__file__)
 
 categories = ['kegg_id', 'name', 'accession', 'chebi_id', 'chemspider_id',
@@ -43,7 +38,7 @@ class HMDB(object):
         self.out_name = os.path.join(self.out_dir, '..', 'data',
                                      'hmdb_dataframe.csv.gz')
 
-    def download_hmdb(self):
+    def _download_hmdb(self):
         """ Downloads hmdb metabolites xml file
 
         :return:
@@ -79,40 +74,39 @@ class HMDB(object):
         print("Downloaded {} and stored {}".format(hmdb_db_url, out_path))
         return
 
-    def unzip_hmdb(self, out_directory):
+    def _unzip_hmdb(self, out_directory):
         """ unzips hmdb metabolites file
 
         :return:
         """
         hmdb_file = os.path.join(self.tmp_dir, self.target_file)
+
         if not os.path.exists(hmdb_file):
             print("Downloading metabolites information from HMDB")
-            self.download_hmdb()
+            self._download_hmdb()
         print("Unzipping metabolites file")
         zip_ref = zipfile.ZipFile(hmdb_file, 'r')
         zip_ref.extractall(out_directory)
         zip_ref.close()
         print("Done unzipping metabolites file")
 
-    def parse_hmdb(self):
+    def setup(self):
         """ parse HMDB to Pandas.DataFrame
 
         """
+        # out_dir = 'c:\users\jamesp~1\\appdata\local\\temp\\tmpnvpdle\HMDB'
         out_dir = os.path.join(self.tmp_dir, 'HMDB')
         if not os.path.exists(out_dir):
             os.mkdir(out_dir)
-            self.unzip_hmdb(out_dir)
+            self._unzip_hmdb(out_dir)
 
         count = 0
 
         print("Parsing metabolites information from files")
-        tmp_all = []
+        tmp_all = [None] * 1000000
+        # tmp_all = []
         for i in os.listdir(out_dir):
             print(i)
-            # if i.startswith('H'):
-                # if count > 100:
-                #     continue
-            count += 1
             filename = os.path.join(out_dir, i)
 
             # get an iterable
@@ -128,17 +122,20 @@ class HMDB(object):
                 if '}' in elem.tag:
                     elem.tag = elem.tag.split('}', 1)[1]
                 if elem.tag == 'metabolite' and event == 'end':
+                    # """
+
+                    # if count > 10000:
+                    #     break
+                    # """
+                    template = _create_dict(elem)
+                    tmp_all[count] = template
+
                     count += 1
-                    if count > 10000:
-                        df = pd.DataFrame(tmp_all, columns=categories)
-                        df.to_csv(self.out_name,
-                                  compression='gzip', index=False)
-                        break
-                    template = create_dict(elem)
-                    # template = check_and_add_to_dict(elem)
-                    tmp_all.append(template)
                     elem.clear()
-        df = pd.DataFrame(tmp_all, columns=categories)
+                    root.clear()
+
+        # df = pd.DataFrame(tmp_all, columns=categories)
+        df = pd.DataFrame(tmp_all[:count], columns=categories)
         df.to_csv(self.out_name,
                   compression='gzip', index=False)
         print("Done processing HMDB")
@@ -147,66 +144,8 @@ class HMDB(object):
         df = pd.read_csv(self.out_name)
         return df
 
-    def setup(self):
-        self.download_hmdb()
-        self.parse_hmdb()
 
-
-def check_and_add_to_dict(elem):
-    reference_dict = XmlDictConfig(elem)
-    template_dict = {}
-    for key in categories:
-        if key in reference_dict:
-            if isinstance(reference_dict[key], str):
-                template_dict[key] = reference_dict[key].encode('ascii',
-                                                                'ignore')
-            elif key == 'protein_associations':
-                tmp_list = []
-                ref_2 = reference_dict[key]
-                if isinstance(ref_2, dict):
-                    if 'protein' in ref_2:
-                        ref_3 = ref_2['protein']
-                        if isinstance(ref_3, XmlListConfig):
-                            for i in ref_3:
-                                if 'gene_name' in i:
-                                    tmp_list.append(i['gene_name'])
-                        elif isinstance(ref_3, XmlDictConfig):
-                            tmp_list.append(ref_3['gene_name'])
-                template_dict[key] = tmp_list
-            elif key == 'synonyms':
-                ref_2 = reference_dict[key]
-                # print(ref_2)
-                if 'synonym' in ref_2:
-                    template_dict[key] = ref_2['synonym']
-                else:
-                    print(ref_2)
-            elif key == 'ontology':
-                ref_2 = reference_dict[key]
-                if 'biofunctions' in ref_2:
-                    ref_3 = ref_2['biofunctions']
-                    if isinstance(ref_3, str):
-                        pass
-                    if isinstance(ref_3, dict):
-                        biofunction = ref_3['biofunction']
-                        if isinstance(biofunction, str):
-                            template_dict['biofunction'] = biofunction.encode(
-                                    'ascii', 'ignore')
-                        elif isinstance(biofunction, XmlListConfig):
-                            template_dict['biofunction'] = list(biofunction)
-                        if 'cellular_locations' in ref_2:
-                            ref_3 = ref_2['cellular_locations']
-                            if 'cellular_location' in ref_3:
-                                template_dict['cellular_location'] = ref_3[
-                                    'cellular_location']
-            elif isinstance(reference_dict[key], dict):
-                print(
-                "{} is of type {}".format(key, type(reference_dict[key])))
-        else:
-            template_dict[key] = None
-    return template_dict
-
-
-def create_dict(elem):
+def _create_dict(elem):
     template = {}
     for i in categories:
         n = elem.find(i)
@@ -248,11 +187,20 @@ def create_dict(elem):
 if __name__ == '__main__':
     import time
 
-    st = time.time()
+    # st = time.time()
     hm = HMDB()
-    hm.download_hmdb()
+    # hm.download_hmdb()
     end = time.time()
-    print(end - st)
-    hm.parse_hmdb()
-    df = hm.load_db()
-    print(df.head(10))
+    # time1 = end - st
+    # print("Download time took {}".format(time1))
+    st = time.time()
+    hm.setup()
+    end = time.time()
+    time2 = end - st
+    print("Parsing time took {}".format(time2))
+    # df = hm.load_db()
+    # print(df.head(10))
+    # 97.2819998264 lxml
+    # 105.569000006 lxml root.clear()
+    # 83.878000021 lxml, no root.clear(), no  elem.getprevious()
+    # 57.4879999161 cElementTree
