b1d565365ea9d8d67d32b0373abbfb6696c95611
James Pino <james.c.pino@vanderbilt.edu>
2017-06-08 11:29:45 -0500

    Removing print statements

--- magine/mappings/hgnc_mapping/create_hgnc_dictionaries.py
+++ magine/mappings/hgnc_mapping/create_hgnc_dictionaries.py
@@ -17,9 +17,6 @@ def download_hgnc():
     print("First time initializing gene_mapper! \n"
           "Downloading from HGNC. Might take awhile.")
     hgnc = pd.read_table(url, delimiter='\t', low_memory=False)
-    print(hgnc.dtypes)
-    print(hgnc['mirbase'].unique())
-    print(hgnc.head(10))
     hgnc = hgnc[hgnc['status'] == 'Approved']
     hgnc = hgnc[['symbol',
                  'uniprot_ids',
@@ -32,7 +29,6 @@ def download_hgnc():
                  'vega_id',
                  'alias_name',
                  'alias_symbol']]
-    print(hgnc.head(10))
     outfile = os.path.join(dirname, '..', 'data', 'hgnc.gz')
     hgnc.to_csv(outfile, compression='gzip', header=True)
     hgnc = pd.read_csv(outfile)
@@ -43,12 +39,8 @@ def download_ncbi():
     url = 'ftp://ftp.ncbi.nlm.nih.gov/gene/DATA/GENE_INFO/Mammalia/Homo_sapiens.gene_info.gz'
     ncbi = pd.read_table(url, delimiter='\t', low_memory=False,
                          compression='gzip')
-    print(ncbi.dtypes)
-    print(ncbi['Symbol'].unique())
-
     cols = ['GeneID', 'Symbol', 'description']
     ncbi = ncbi[cols]
-    print(ncbi.head(10))
     outfile = os.path.join(dirname, '..', 'data', 'ncbi.gz')
     ncbi.to_csv(outfile, compression='gzip', header=True)
 
