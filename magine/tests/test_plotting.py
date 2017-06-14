73571e55917ecdcb9ec2ada39b19ba34ccd3193e
James Pino <james.c.pino@vanderbilt.edu>
2017-06-14 15:48:16 -0500

    Refactored tests to stop writing output to test directory.

--- magine/tests/test_plotting.py
+++ magine/tests/test_plotting.py
@@ -1,80 +1,70 @@
 import os
 import pandas as pd
-
+import tempfile
 from magine.plotting.venn_diagram_maker import create_venn3, create_venn2
 from magine.plotting.volcano_plots import volcano_plot
 from magine.plotting.species_plotting import plot_dataframe, \
     plot_list_of_genes2
 
 
-def test_volcano():
-    d_name = os.path.join(os.path.dirname(__file__), 'Data',
-                          'example_apoptosis.csv')
-    data = pd.read_csv(d_name)
-    volcano_plot(data=data, save_name='volcano_test')
-    volcano_plot(data=data, save_name='volcano_test',
-                 bh_criteria=True, y_range=[0, 5], x_range=[-10, 10])
+class TestSpeciesPlotting(object):
+    def setUp(self):
+        d_name = os.path.join(os.path.dirname(__file__), 'Data',
+                              'example_apoptosis.csv')
+        self.data = pd.read_csv(d_name)
+        self.data['compound_id'] = self.data['compound']
+        self.data['time_points'] = self.data['time']
+        self.out_dir = tempfile.mkdtemp()
+
+    def test_plotly(self):
+        """
+        tests plotly offline graph creation from a pandas.dataframe
+        Returns
+        -------
+
+        """
+        list_species = ['PARP1', 'TP53']
+        plot_list_of_genes2(self.data, list_of_genes=list_species,
+                            out_dir=self.out_dir, save_name='test_me2',
+                            plot_type='plotly',
+                            )
+
+    def test_matplotlib(self):
+        """
+        test matplotlib graph creation from a pandas.dataframe
+        Returns
+        -------
+
+        """
+        list_species = ['PARP1', 'TP53']
+        plot_list_of_genes2(self.data, list_of_genes=list_species,
+                            out_dir=self.out_dir, save_name='test_me',
+                            plot_type='matplotlib')
+
+    def test_plot_df(self):
+        plot_dataframe(exp_data=self.data,
+                       species_type='proteins',
+                       html_filename='test',
+                       out_dir=self.out_dir,
+                       plot_type='plotly'
+                       )
+
+    def test_volcano(self):
+        volcano_plot(data=self.data, save_name='volcano_test')
+        volcano_plot(data=self.data, save_name='volcano_test',
+                     bh_criteria=True, y_range=[0, 5], x_range=[-10, 10])
 
 
-def test_plotly():
-    """
-    tests plotly offline graph creation from a pandas.dataframe
-    Returns
-    -------
+class TestVennDiagram(object):
+    def setUp(self):
+        self.x = ['A', 'B', 'C', 'D']
+        self.y = ['C', 'D', 'E', 'F']
+        self.z = ['D', 'E', 'F', 'N', 'Z', 'A']
+        self.out_dir = tempfile.mkdtemp()
 
-    """
-    d_name = os.path.join(os.path.dirname(__file__), 'Data',
-                          'example_apoptosis.csv')
+    def test_venn_2(self):
+        create_venn2(self.x, self.y, 'X', 'Y', 'test_1')
 
-    d = pd.read_csv(d_name)
+    def test_venn_3(self):
+        create_venn3(self.x, self.y, self.z, 'X', 'Y', 'z', 'test_1')
 
-    list_species = ['PARP1', 'TP53']
-    plot_list_of_genes2(d, list_of_genes=list_species,
-                        out_dir='TEST_DELETE', save_name='test_me2',
-                        plot_type='plotly',
-                        )
-
-
-def test_matplotlib():
-    """
-    test matplotlib graph creation from a pandas.dataframe
-    Returns
-    -------
-
-    """
-    d_name = os.path.join(os.path.dirname(__file__), 'Data',
-                          'example_apoptosis.csv')
-
-    d = pd.read_csv(d_name)
-    list_species = ['PARP1', 'TP53']
-    plot_list_of_genes2(d, list_of_genes=list_species,
-                        out_dir='DELETE', save_name='test_me',
-                        plot_type='matplotlib')
-
-
-def test_plot_df():
-    d_name = os.path.join(os.path.dirname(__file__), 'Data',
-                          'example_apoptosis.csv')
-    d = pd.read_csv(d_name)
-    d['compound_id'] = d['compound']
-    d['time_points'] = d['time']
-    plot_dataframe(d, 'test_df_out', 'test_df_out', 'plotly')
-
-
-x = ['A', 'B', 'C', 'D']
-y = ['C', 'D', 'E', 'F']
-z = ['D', 'E', 'F', 'N', 'Z']
-
-
-def test_venn_2():
-    create_venn2(x, y, 'X', 'Y', 'test_1')
-
-
-def test_venn_3():
-    create_venn3(x, y, z, 'X', 'Y', 'z', 'test_1')
-
-
-if __name__ == '__main__':
-    test_plotly()
-    test_matplotlib()
-    test_plot_df()
