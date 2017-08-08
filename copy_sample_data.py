2c42b3fe9381d82f0685fab617d60630a9addd55
James Pino <james.c.pino@vanderbilt.edu>
2017-08-08 16:34:06 -0500

    Adding create empty directories to script.

--- copy_sample_data.py
+++ copy_sample_data.py
@@ -5,7 +5,15 @@ from appdirs import user_data_dir
 
 dir_name = user_data_dir('magine')
 
+# check or create the main data storage directory
+if not os.path.exists(dir_name):
+    os.makedirs(dir_name)
+
+
 id_mapping_dir = os.path.join(dir_name, 'id_data')
+# check or create the network data storage directory
+if not os.path.exists(id_mapping_dir):
+    os.makedirs(id_mapping_dir)
 
 _sample_dir = os.path.join(os.path.dirname(__file__), '_sample_databases')
 
