72bc41cca006c19a0f2bd87f1fd2baea70c8ec9c
James Pino <james.c.pino@vanderbilt.edu>
2017-08-20 13:36:54 -0500

    Adding a check if api returns int. Not sure why Reactome contentservice is not working for some things now. #TODO check later

--- magine/networks/databases/reactome_api.py
+++ magine/networks/databases/reactome_api.py
@@ -1,5 +1,5 @@
-from bioservices.services import REST
 import pandas as pd
+from bioservices.services import REST
 
 
 class Reactome(REST):
@@ -8,7 +8,8 @@ class Reactome(REST):
 
     """
 
-    _url = "http://reactomews.oicr.on.ca:8080/ReactomeRESTfulAPI/RESTfulWS"
+    # _url = "http://reactomews.oicr.on.ca:8080/ReactomeRESTfulAPI/RESTfulWS"
+    _url = 'http://reactome.org/ContentService/data/'
 
     def __init__(self, verbose=True, cache=False):
         super(Reactome, self).__init__("Reactome(URL)", url=Reactome._url,
@@ -18,6 +19,7 @@ class Reactome(REST):
 
         # for buffering
         self._list_pathways = []
+        # http://reactome.org/ContentService/
         self._content_url = 'http://reactome.org/ContentService/data/'
 
     def get_reaction_info(self, reaction):
@@ -26,8 +28,15 @@ class Reactome(REST):
         .. note:: draft version
         """
         q = 'query/{}'.format(reaction)
-        res = self.http_get(self._content_url + q, frmt='json')
-        return res
+        return self._get(q, 'json')
+
+    def get_reaction_detail(self, reaction):
+        """Fetch information from the reaction HTML page
+
+        .. note:: draft version
+        """
+        q = 'query/{}'.format(reaction)
+        return self._get(q, 'json')
 
     def get_entity_info(self, entity):
         """Fetch information from the reaction HTML page
@@ -35,8 +44,7 @@ class Reactome(REST):
         .. note:: draft version
         """
         q = 'query/{}/extended'.format(entity)
-        res = self.http_get(self._content_url+q, frmt='json')
-        return res
+        return self._get(q, 'json')
 
     def get_entity_attribute(self, entity, attribute):
         """Fetch information from the reaction HTML page
@@ -44,19 +52,15 @@ class Reactome(REST):
         .. note:: draft version
         """
         q = 'query/{}/{}'.format(entity, attribute)
-        res = self.http_get(self._content_url+q, frmt='string')
-
-        return res
+        return self._get(q, 'string')
 
     def get_event_participants(self, event):
         q = 'event/{}/participants'.format(event)
-        res = self.http_get(self._content_url+q, frmt='json')
-        return res
+        return self._get(q, 'json')
 
     def get_event_participating_phys_entities(self, event):
         q = 'event/{}/participatingPhysicalEntities'.format(event)
-        res = self.http_get(self._content_url+q, frmt='json')
-        return res
+        return self._get(q, 'json')
 
     def get_pathway(self, pathway_id):
         """Fetch information from the reaction HTML page
@@ -64,9 +68,7 @@ class Reactome(REST):
         .. note:: draft version
         """
         q = 'pathway/{}/Complex'.format(pathway_id)
-        res = self.http_get(self._content_url +q, frmt='json')
-
-        return res
+        return self._get(q, 'json')
 
     def get_pathway_events(self, pathway_id):
         """Fetch information from the reaction HTML page
@@ -74,168 +76,10 @@ class Reactome(REST):
         .. note:: draft version
         """
         q = 'pathway/{}/containedEvents'.format(pathway_id)
-        res = self.http_get(self._content_url + q, frmt='json')
-        return res
+        return self._get(q, 'json')
 
-
-'''
-    def _download_list_pathways(self):
-        url = "http://www._reactome.org/download/current/ReactomePathways.txt"
-        if len(self._list_pathways) == 0:
-            res = self.session.get(url)
-            if res.status_code == 200:
-                res = res.text  # content does not work in python 3.3
-                res = res.strip()
-                self._list_pathways = [x.split("\t") for x in res.split("\n")]
-            else:
-                self.logging.error("could not fetch the pathways")
-        return self._list_pathways
-
-    def get_list_pathways(self):
-        """Return list of pathways from _reactome website
-
-        :return: list of lists. Each sub-lis contains 3 items: _reactome pathway
-            identifier, description and species
-
-        """
-        res = self._download_list_pathways()
-        return res
-
-    def get_species(self):
-        """Return list of species from all pathways"""
-        res = set([x[2] for x in self.get_list_pathways()])
-        return res
-
-    def pathway_participants(self, identifier):
-        """Get list of pathway participants for a pathway using
-
-        :param int identifier: Pathway database identifier
-        :return: list of fully encoded PhysicalEntity objects in the pathway
-            (in JSON)
-
-        """
-        res = self.http_get("pathwayParticipants/{0}".format(identifier),
-                            frmt='json')
-        return res
-
-    def pathway_complexes(self, identifier):
-        """Get complexes belonging to a pathway
-
-        :param int identifier: Pathway database identifier
-        :return: list of all PhysicalEntity objects that participate in the
-            Pathway.(in JSON)
-
-
-        """
-        res = self.http_get("pathwayComplexes/{0}".format(identifier),
-                            frmt="json")
-        return res
-
-    def query_by_id(self, classname, identifier):
-        """Get Reactome Database for a specific object.
-
-
-        :param str classname: e.g. Pathway
-        :param int identifier: database identifier or stable identifier if available
-
-        It returns a full object, including full class information about
-        all the attributes of the returned object. For example, if the object has
-        one PublicationSource attribute, it will return a full PublicationSource
-        object within the returned object.
-        """
-        url = "queryById/{0}/{1}".format(classname, identifier)
-        res = self.http_get(url, frmt='json')
-        return res
-
-    def query_by_ids(self, classname, identifiers):
-        """
-
-        :param str classname: e.g. Pathway
-        :param list identifiers: list of strings or int
-
-        .. warning:: not sure the wrapping is correct
-        """
-
-        identifiers = self.devtools.list2string(identifiers)
-        url = "queryByIds/{0}".format(classname)
-        res = self.http_post(url, frmt="json", data=identifiers)
-        # headers={'Content-Type': "application/json"})
-        return res
-
-    def query_hit_pathways(self, query):
-        """Get pathways that contain one or more genes passed in the query list.
-
-        In the Reactome data model, pathways are organized in a
-        hierarchical structure. The returned pathways in this method are pathways
-        having detailed manually drawn pathway diagrams. Currently only human
-        pathways will be returned from this method.
-
-
-        """
-        identifiers = self.devtools.list2string(query)
-        res = self.http_post("queryHitPathways", frmt='json', data=identifiers)
-        return res
-
-    def query_pathway_for_entities(self, identifiers):
-        """Get pathway objects by specifying an array of PhysicalEntity database identifiers.
-
-
-        The returned Pathways should
-        contain the passed EventEntity objects. All passed EventEntity database
-        identifiers should be in the database.
-
-        """
-        identifiers = self.devtools.list2string(identifiers, space=False)
-        url = "pathwayForEntities"
-        res = self.http_post(url, frmt='json', data={'ID': identifiers})
-        return res
-
-    def species_list(self):
-        """Get the list of species used Reactome"""
-        url = "speciesList"
-        res = self.http_get(url, frmt='json')
-        return res
-
-    def SBML_exporter(self, identifier):
-        """Get the SBML XML text of a pathway identifier
-
-        :param int identifier: Pathway database identifier
-        :return: SBML object in XML format as a string
-
-
-        """
-        url = "sbmlExporter/{0}".format(identifier)
-        res = self.http_get(url, frmt='xml')
-        return res
-
-    def get_all_reactions(self):
-        """Return list of reactions from the Pathway"""
-        res = self.get_list_pathways()
-        return [x[0] for x in res]
-
-    def bioservices_get_reactants_from_reaction_identifier(self, reaction):
-        """Fetch information from the reaction HTML page
-
-        .. note:: draft version
-        """
-        res = self.http_get(
-                'http://www._reactome.org/content/detail/%s' % reaction)
-        res = res.content
-
-        try:
-            reactants = [x for x in res.split("\n") if '<title>' in x]
-            reactants = reactants[0].split("|")[1].strip().strip('</title>')
-        except  Exception as err:
-            print('Could not interpret title')
-            return res
-
-        if reactants.count(':') == 1:
-            reactants = reactants.split(":")
-        else:
-            pass
-
-        return reactants
-'''
+    def _get(self, q, fmt):
+        return self.http_get(q, frmt=fmt)
 
 pd.set_option('expand_frame_repr', False)
 pd.set_option('display.width', 10000)
@@ -315,8 +159,11 @@ def extract_list_from_key(keyword, y):
 
 
 def get_entity_info(species):
-    x = _reactome.get_entity_info(species)
 
+    x = _reactome.get_entity_info(species)
+    if isinstance(x, int):
+        print("Here lies an error {}".format(species))
+        return dict()
     info_needed = ['referenceType', 'className', 'startCoordinate',
                    'endCoordinate', 'referenceEntity', 'compartment',
                    'referenceEntity', 'displayName', 'hasModifiedResidue']
@@ -359,72 +206,27 @@ def get_entity_info(species):
                                 mods.append(ms['dbId'])
                             else:
                                 for m_type in ms:
-                                    print(m_type, ms[m_type])
+                                    print(
+                                    "hasModifiedResidue", m_type, ms[m_type])
             for m in x[n]:
                 if m in info_needed:
                     entity_info[m] = x[n][m]
                 elif m not in dont_need:
-                    print(species, m, x[n][m])
+                    print("databaseObject", species, m, x[n][m])
     return entity_info
-
-
-def count_entites(list_of_object):
-    count_dict = {}
-    name_dict = {}
-    for i in list_of_object:
-        if i in count_dict:
-            count_dict[i] += 1
-        else:
-            count_dict[i] = 1
-    for i in count_dict:
-        x = _reactome.get_entity_attribute(i, 'displayName')
-        name_dict[i] = x
-        # get_entity_info(i)
-    return name_dict, count_dict
 
 
 def create_graph(inputs, outputs, class_names, catalyst, rxn_name, graph,
                  prev_events):
-    for each in inputs:
-        name = each
-        display = inputs[each]
+    for name in inputs:
+        display = inputs[name]
         graph.add_node(name,
                        label=display,
                        displayName=display,
                        dbId=name,
-                       shape=shapes[class_names[each]])
+                       shape=shapes[class_names[name]])
         if catalyst:
-            if each == catalyst:
-                pass
-            else:
-                graph.add_edge(catalyst, rxn_name)
-        graph.add_edge(name, rxn_name)
-        for i in prev_events:
-            graph.add_edge(i, name)
-
-    for each in outputs:
-        name = each
-        display = outputs[each]
-        graph.add_node(name,
-                       label=display,
-                       displayName=display,
-                       dbId=name,
-                       shape=shapes[class_names[each]])
-        graph.add_edge(rxn_name, each)
-
-
-def create_graph_from_df(inputs, outputs, class_names, catalyst, rxn_name,
-                         graph, prev_events):
-    for each in inputs:
-        name = each
-        display = inputs[each]
-        graph.add_node(name,
-                       label=display,
-                       displayName=display,
-                       dbId=name,
-                       shape=shapes[class_names[each]])
-        if catalyst:
-            if each == catalyst:
+            if name == catalyst:
                 pass
             else:
                 graph.add_edge(catalyst, rxn_name)
@@ -476,7 +278,8 @@ def get_reaction_info(reaction, graph):
         return None, None
 
     # get all reaction info
-    y = _reactome.get_reaction_info(reaction['dbId'])
+    rxn_name = reaction['dbId']
+    y = _reactome.get_reaction_info(rxn_name)
 
     # check to see if compartment exists
     if 'compartment' not in y:
@@ -491,7 +294,6 @@ def get_reaction_info(reaction, graph):
         return None, None
 
     # get reaction id and name
-    rxn_name = reaction['dbId']
     rxn_display_name = reaction['displayName']
 
     if verbose:
