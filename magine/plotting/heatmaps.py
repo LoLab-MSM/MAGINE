d3c67ce49886e9f3f41b5f195868e32cabc2fb91
James Pino <james.c.pino@vanderbilt.edu>
2018-03-27 20:04:38 -0500

    Adding mark annotations to heatmaps.

--- magine/plotting/heatmaps.py
+++ magine/plotting/heatmaps.py
@@ -4,11 +4,11 @@ import seaborn as sns
 from magine.data.tools import pivot_table
 
 
-def heatmap_from_array(data, convert_to_log=False, yticklabels='auto',
+def heatmap_from_array(data, convert_to_log=False, y_tick_labels='auto',
                        cluster_row=False, cluster_col=False,
                        columns='sample_id', index='term_name',
                        values='combined_score', div_colors=False, num_colors=7,
-                       fig_size=(6, 4)):
+                       fig_size=(6, 4), rank_index=False, annotate_sig=False):
     """
 
     Parameters
@@ -17,7 +17,7 @@ def heatmap_from_array(data, convert_to_log=False, yticklabels='auto',
 
     data : pandas.DataFrame
     convert_to_log : bool
-    yticklabels : list_like
+    y_tick_labels : list_like
     columns : str
         Name of columns of df for pivotn
     index : str
@@ -37,7 +37,31 @@ def heatmap_from_array(data, convert_to_log=False, yticklabels='auto',
 
     """
     array = pivot_table(data, convert_to_log, columns=columns, index=index,
-                        values=values)
+                        fill_value=0.0, values=values)
+    labels = None
+    fmt = None
+
+    if rank_index:
+        array.sort_index(ascending=True, inplace=True)
+
+    if annotate_sig:
+        # Have to rank by column for this to work
+        if 'significant_flag' in data.columns:
+            tmp2 = pivot_table(data, False, columns=columns,
+                               index=index, fill_value=False,
+                               values='significant_flag', min_sig=False)
+
+            tmp2 = tmp2.reindex(array.index)
+            tmp2[tmp2 > 0] = True
+            tmp2 = tmp2.replace(False, '')
+            tmp2 = tmp2.replace(True, '*')
+
+            labels = tmp2.as_matrix()
+            fmt = ''
+        else:
+            annotate_sig = False
+            print("To annotate please add a significant_flag column to data")
+
     if div_colors:
         pal = sns.color_palette("coolwarm", num_colors)
         center = 0
@@ -46,13 +70,25 @@ def heatmap_from_array(data, convert_to_log=False, yticklabels='auto',
         center = None
 
     if cluster_col or cluster_row:
-        fig = sns.clustermap(array, yticklabels=yticklabels, figsize=fig_size,
-                             col_cluster=cluster_col, row_cluster=cluster_row,
-                             center=center, cmap=pal)
+        fig = sns.clustermap(array, yticklabels=y_tick_labels,
+                             figsize=fig_size, col_cluster=cluster_col,
+                             row_cluster=cluster_row, center=center, cmap=pal)
+
+        if cluster_row and annotate_sig:
+            labels = labels[fig.dendrogram_row.reordered_ind]
+        if cluster_col and annotate_sig:
+            labels = labels[:, fig.dendrogram_col.reordered_ind]
+        if (cluster_col or cluster_row) and annotate_sig:
+            plt.close()
+            fig = sns.clustermap(array, yticklabels=y_tick_labels,
+                                 figsize=fig_size, col_cluster=cluster_col,
+                                 row_cluster=cluster_row, center=center,
+                                 cmap=pal, annot=labels, fmt=fmt)
+
     else:
         fig = plt.figure(figsize=fig_size)
         ax = fig.add_subplot(111)
-        sns.heatmap(array, ax=ax, yticklabels=yticklabels, cmap=pal,
-                    center=center)
+        sns.heatmap(array, ax=ax, yticklabels=y_tick_labels, cmap=pal,
+                    center=center, annot=labels, fmt=fmt)
 
     return fig
