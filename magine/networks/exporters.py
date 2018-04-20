74c3a32f84557dd0311cd87ff25b1eb88bf58f52
James Pino <james.c.pino@vanderbilt.edu>
2018-04-20 17:26:58 -0500

    Adding exporter.py, which was named utils before.

---
+++ magine/networks/exporters.py
@@ -0,0 +1,156 @@
+import json
+import tempfile
+
+import networkx as nx
+
+
+def nx_to_igraph(network):
+    try:
+        import igraph
+    except ImportError:
+        raise ImportError('requires igraph ',
+                          'http://pygraphviz.github.io/')
+    file_descriptor, file_path = tempfile.mkstemp(suffix='.gml')
+
+    nx.write_gml(network, file_path)
+    igraph_network = igraph.read(file_path)
+    return igraph_network
+
+
+def nx_to_json(network):
+    """
+    Convert from networkx Digraph to cytoscape json format
+    Parameters
+    ----------
+    network : nx.DiGraph
+
+    Returns
+    -------
+    dict
+    """
+    nodes, edges = [], []
+
+    for node_id in network.nodes_iter():
+        node = network.node[node_id]
+        new_node = {}
+        node_columns = node.keys()
+        data = {col: node[col] for col in node_columns if col != 0}
+        if 'position' in node.keys():
+            new_node['position'] = node['position']
+        data['id'] = str(node_id)
+        data['name'] = data['id']
+        new_node['data'] = data
+        nodes.append(new_node)
+
+    for source, target, data in network.edges_iter(data=True):
+        data['source'] = str(source)
+        data['target'] = str(target)
+        edges.append({'data': data})
+
+    return {'nodes': json.dumps(nodes), 'edges': json.dumps(edges)}
+
+
+def nx_to_dot(graph):
+    try:
+        import pygraphviz
+    except ImportError:
+        raise ImportError('requires pygraphviz ',
+                          'http://pygraphviz.github.io/')
+
+    graph = format_to_directions(graph)
+
+    directed = graph.is_directed()
+    strict = graph.number_of_selfloops() == 0 and not graph.is_multigraph()
+    new_g = pygraphviz.AGraph(name=graph.name,
+                              strict=strict,
+                              directed=directed,
+                              encoding='utf8')
+    # default graph attributes
+    new_g.graph_attr.update(graph.graph.get('graph', {}))
+    new_g.node_attr.update(graph.graph.get('node', {}))
+    new_g.edge_attr.update(graph.graph.get('edge', {}))
+
+    # add nodes
+    for n, data in graph.nodes(data=True):
+        new_g.add_node(n, **data)
+
+    # loop over edges
+    for u, v, data in graph.edges_iter(data=True):
+        new_g.add_edge(u, v, **dict((k, str(v)) for k, v in data.items()))
+    return new_g
+
+
+def export_to_dot(graph, save_name, image_format='png', engine='dot',
+                  dpi=200, concentrate=False):
+    """
+    Converts networkx graph to graphviz dot
+
+    Parameters
+    ----------
+    graph : networkx.DiGraph
+    save_name : str
+        name of file to save
+    image_format : str
+        format of output( pdf, png, svg)
+    engine : str
+        graphviz engine
+            dot, twopi,
+    dpi: int
+        resolution of figure
+    concentrate : bool
+        Concentrate bi-edges into one edge
+
+    Returns
+    -------
+
+    """
+
+    py_dot = nx_to_dot(graph)
+    if save_name.endswith('.png'):
+        basename = save_name[:-4]
+    else:
+        basename = save_name
+    py_dot.write('{}.dot'.format(basename))
+    arg = '-Gdpi={} -Gconcentrate={}'.format(
+        dpi, 'true' if concentrate else 'false'
+    )
+    py_dot.draw('{}.{}'.format(basename, image_format), prog=engine, args=arg)
+
+
+def check_graphviz(network):
+    if isinstance(network, nx.DiGraph):
+        network = format_to_directions(network)
+
+    return nx_to_dot(network)
+
+
+def format_to_directions(network):
+    activators = ['activate', 'expression', 'phosphorylate']
+    inhibitors = [
+        'inhibit', 'repression', 'dephosphorylate', 'deubiquitinate',
+        'ubiquitinate'
+    ]
+    physical_contact = ['binding', 'dissociation', 'stateChange',
+                        'compound', 'glycosylation']
+    indirect_types = ['indirect']
+    for source, target, data in network.edges(data=True):
+        if 'interactionType' in data:
+            edge_type = data['interactionType']
+            for j in activators:
+                if j in edge_type:
+                    network[source][target]['arrowhead'] = 'normal'
+            for j in inhibitors:
+                if j in edge_type:
+                    network[source][target]['arrowhead'] = 'tee'
+
+            for j in physical_contact:
+                if j in edge_type:
+                    network[source][target]['dir'] = 'both'
+                    network[source][target]['arrowtail'] = 'diamond'
+                    network[source][target]['arrowhead'] = 'diamond'
+
+            for j in indirect_types:
+                if j in edge_type:
+                    network[source][target]['arrowhead'] = 'diamond'
+                    network[source][target]['style'] = 'dashed'
+    return network
--- scripts/raptr_to_enrichment.py
+++ scripts/raptr_to_enrichment.py
@@ -1,18 +1,55 @@
 import sys
 
-from magine.data.datatypes import ExperimentalData
-from magine.data.formatter import process_raptr_zip
-from magine.ontology.enrichr import run_enrichment_for_project
+import pandas as pd
+
+import magine.enrichment.enrichment_tools as et
 
 
-def run(zip_file, save_name):
-    df = process_raptr_zip(zip_file)
-    exp_data = ExperimentalData(df)
-    run_enrichment_for_project(exp_data, save_name)
+def return_table_from_model(save_name, project_name, category, dbs):
+    cols = ['term_name', 'combined_score', 'adj_p_value', 'rank', 'genes',
+            'n_genes', 'sample_id', 'db', 'category']
+
+    project_name = project_name.split(',')
+    category = category.split(',')
+    dbs = dbs.split(',')
+    if len(project_name) > 1:
+        cols.insert(0, 'project_name')
+
+    df = pd.read_csv(save_name)
+    if isinstance(project_name, str):
+        df = df[df['project_name'] == project_name]
+    elif isinstance(project_name, list):
+        df = df[df['project_name'].isin(project_name)]
+
+    df = df.filter(category__in=category)
+    df = df.filter(db__in=dbs)
+    df = pd.DataFrame(list(df.values()))[cols]
+
+    df = et.filter_dataframe(df,
+                             p_value=0.2,
+                             combined_score=0,
+                             db=dbs,
+                             category=category)
+
+    tmp_table = df.copy()
+    tmp_table['genes'] = tmp_table['genes'].str.split(',').str.join(', ')
+
+    tmp_table = tmp_table[cols]
+    data = tmp_table.to_dict('split')
+    template_vars = {"data": data}
+    return template_vars
+
+
+_help = """
+please provide a savename, project_name, category, and dbs
+
+All are expected to be comma delimited if more than one choice
+"""
+
 
 
 if __name__ == '__main__':
     print(sys.argv)
-    assert len(sys.argv) == 3, 'please provide a zipfile and a savename'
+    assert len(sys.argv) == 3, _help
 
-    run(sys.argv[1], sys.argv[2])
+    return_table_from_model(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
