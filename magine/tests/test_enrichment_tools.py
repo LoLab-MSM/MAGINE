6e99870eb7547a0f18032e00c9ce50a5f01af64e
James Pino <james.c.pino@vanderbilt.edu>
2018-04-23 14:52:40 -0500

    Adding more tests for enrichment_tools.

--- magine/tests/test_enrichment_tools.py
+++ magine/tests/test_enrichment_tools.py
@@ -8,8 +8,6 @@ data_dir = os.path.dirname(__file__)
 
 df = pd.read_csv(os.path.join(data_dir, 'Data', 'enrichr_test_enrichr.csv'))
 
-print(df.head(10))
-
 
 def test_filter_row():
     terms = ['apoptotic process',
@@ -24,4 +22,38 @@ def test_filter_row():
     assert slimmed.shape[0] == 111
 
 
-test_filter_row()
+def test_jaccard_index():
+    term1 = ['BAX', 'BCL2', 'MCL1', 'CASP3']
+    term2 = ['BAX', 'BCL2', 'MCL1', 'TP53']
+    score = et.jaccard_index(term1, term2)
+
+    assert score == 0.6
+
+
+def test_filter_sim_terms():
+    slimmed = et.filter_similar_terms(df)
+    assert slimmed.shape == (3, 10)
+
+    sim2 = et.remove_redundant(df)
+    assert sim2.shape == (0, 10)
+
+
+def test_genes_from_df():
+    genes = et.all_genes_from_df(df)
+    assert len(genes) == 5
+
+
+def test_find_similar_terms():
+    sim = et.find_similar_terms('apoptotic process', df)
+
+
+def test_dist():
+    import matplotlib.figure
+    dist = et.dist_matrix(df)
+    assert isinstance(dist, matplotlib.figure.Figure)
+
+
+def test_dist2():
+    import matplotlib.figure
+    dist = et.dist_matrix2(df)
+    assert isinstance(dist, matplotlib.figure.Figure)
