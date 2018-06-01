71c6a602e8d61567732440f16716ceb73e39f596
James Pino <james.c.pino@vanderbilt.edu>
2018-06-01 11:44:41 -0500

    Rearranging enrichr source file. Moving main class to front, deleting old list of ontology sets.
    Removing goatools as dependency.
    
    Mocking py2cytoscape from docs (no need to try to build igraph).

--- magine/html_templates/html_tools.py
+++ magine/html_templates/html_tools.py
@@ -142,8 +142,6 @@ def _format_simple_table(data):
                   'treated_control_fold_change']
 
     int_type = ['n_genes', 'rank']
-    # tmp_table['term_name'] = tmp_table['term_name'].str.replace('/', '\\')
-    # tmp_table['term_name'] = tmp_table['term_name'].str.replace("'", '\'')
     for i in data.columns:
         if i in float_type:
             tmp_table[i] = tmp_table[i].fillna(0)
