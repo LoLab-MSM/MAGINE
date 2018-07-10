febc693e04d6a4c1960790b3476fee33b78bda0b
James Pino <james.c.pino@vanderbilt.edu>
2018-07-10 12:14:25 -0700

    Removing indirect edges from kegg.

--- magine/networks/databases/__init__.py
+++ magine/networks/databases/__init__.py
@@ -1,10 +1,10 @@
 from .biogrid_interactions import load_biogrid_network, download_biogrid
-from .kegg_kgml import download_kegg
+from .kegg_kgml import load_all_of_kegg, load_kegg_mappings
 from .reactome_functional_interaction import load_reactome_fi, \
     download_reactome_fi
 from .signor import load_signor, download_signor
 
 __all__ = ['download_reactome_fi', 'load_reactome_fi',
            'load_biogrid_network', 'download_biogrid',
-           'download_kegg',
+           'load_kegg_mappings', 'load_all_of_kegg',
            'load_signor', 'download_signor']
