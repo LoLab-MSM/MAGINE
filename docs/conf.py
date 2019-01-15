c4ca7b323c8ed56c2129c8e981103ca283d12b46
jamespino <james.c.pino@vanderbilt.edu>
2019-01-14 19:30:30 -0600

    Removing sphinx mathjax from conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ extensions = [
     'sphinx.ext.todo',
     'numpydoc',
     'nbsphinx',
-    'sphinx.ext.mathjax',
+    # 'sphinx.ext.mathjax',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
